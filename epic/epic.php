c709d151998d1799bc9c876f3ac7c2e92c2f4565
Victoria Chacon <Vchaco06@gmail.com>
2016-07-29 14:30:26 -0600

    moved epic.html to epic.php

---
+++ epic/epic.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang=" 'en">
+	<head>
+		<meta charset="utf 8"/>
+		<title>CrumbTrail</title>
+	</head>
+	<body>
+		<main>
+			<header>
+				<h1>CrumbTrail: Documentation</h1>
+			</header>
+			<h2>CrumbTrail Goals</h2>
+			<p>User Side: </p>
+			<p>Truck Side:</p>
+
+		</main>
+	</body>
+</html>
