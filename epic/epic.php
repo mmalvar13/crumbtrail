09b23640a4e2524c8e7d09aaddb2a9d9c31f8445
Monica Alvarez <mmalvar13@gmail.com>
2016-07-29 14:36:56 -0600

    added crumbtrails main goal

--- epic/epic.php
+++ epic/epic.php
@@ -10,6 +10,9 @@
 				<h1>CrumbTrail: Documentation</h1>
 			</header>
 			<h2>CrumbTrail Goals</h2>
+			<ul>
+				<li><strong>Crumbtrail allows food trucks to share their location and allows users to find food trucks that are serving now</strong></li>
+			</ul>
 			<p>User Side: </p>
 			<p>Truck Side:</p>
 
