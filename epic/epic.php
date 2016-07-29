d46b87d3909e661a18e146dd68276c7d2d19880e
Victoria Chacon <Vchaco06@gmail.com>
2016-07-29 14:41:09 -0600

    Added a css file. This can be worked on as we gain our knowledge of CSS.

--- epic/epic.php
+++ epic/epic.php
@@ -2,14 +2,14 @@
 <html lang=" 'en">
 	<head>
 		<meta charset="utf 8"/>
-		<title>CrumbTrail</title>
+		<title>Crumbtrail</title>
 	</head>
 	<body>
 		<main>
 			<header>
-				<h1>CrumbTrail: Documentation</h1>
+				<h1>Crumbtrail: Documentation</h1>
 			</header>
-			<h2>CrumbTrail Goals</h2>
+			<h2>Crumbtrail Goals</h2>
 			<ul>
 				<li><strong>Crumbtrail allows food trucks to share their location and allows users to find food trucks that are serving now</strong></li>
 			</ul>
