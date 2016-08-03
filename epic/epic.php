b5a3bd4f700e534a8160894b7500b977b974a04d
Monica Alvarez <mmalvar13@gmail.com>
2016-08-03 08:21:52 -0600

    Persona for Company Admin-Interaction Flow 2

--- epic/epic.php
+++ epic/epic.php
@@ -165,6 +165,16 @@
 				<li>take to confirmation page that tells them their login information has been sent to a CrumbTrail admin to be confirmed, and to check their email in the next 48 hours</li>
 			</ol>
 			<h4>Goal: As a food truck owner, I have arrived at the location I am going to serve at and would like to share my location on CrumbTrail</h4>
+			<ol>
+				<li>open app, or type in url</li>
+				<li>Opens to user's home map page. Push "Sign In" in the top right corner</li>
+				<li>at sign in page, user enters their personal email address and password that they registered with, and push "log in"</li>
+				<li>User is now at the food truck home page, which is a map with their current location pinged, and the pinged locations of other active food trucks in the area. Click the button "Serving Now"</li>
+				<li>The user is prompted to enter location (can enter one or use current location), end time, and which truck is out. User enters this information and clicks “ok”.
+				</li>
+				<li>When employee is done, user clicks “stop serving” button, or it is automatically shut off after the input end time
+				</li>
+			</ol>
 
 			<hr>
 			<h2>Persona for company employee</h2>
