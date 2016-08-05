bfe2e00faf8e38234246a7d0e59f2b6375a87682
baca.loren@gmail.com <baca.loren@gmail.com>
2016-08-05 11:11:04 -0600

    finished elements for profile table in SQL https://app.asana.com/0/161555263951748/164422210524634

--- sql/crumbtrail.sql
+++ sql/crumbtrail.sql
@@ -7,22 +7,31 @@ DROP TABLE IF EXISTS ?????;
 DROP TABLE IF EXISTS ?????;
 
 
-CREATE TABLE profile(
+CREATE TABLE profile (
 	-- always start with your primary keys first!
 	-- make it of size and type INT, give it auto increment because it's a primary key, and require a value so use NOT NULL
 
-	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileId              INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	-- now create other attributes within profileId
-	profileName VARCHAR(128) NOT NULL,
-	profileEmail VARCHAR(128) NOT NULL ,
-	profilePhone VARCHAR(32) NOT NULL ,
+	profileName            VARCHAR(128)                NOT NULL,
+	profileEmail           VARCHAR(128)                NOT NULL,
+	profilePhone           VARCHAR(32)                 NOT NULL,
 
 	profileActivationToken VARCHAR(128), -- look into this one,  see if data type and initial Null value is ok. ask dylan if correct!!!*****
 
-	profileType CHAR(1), -- to denote the 3 types of profiles (A-admin, O-owner, E-employee) look into this!!!!!!
+	profileType            CHAR(1), -- to denote the 3 types of profiles (A-admin, O-owner, E-employee) look into this!!!!!!
 
-	profileHash CHAR(128) NOT NULL,
-	profileSalt CHAR(64) NOT NULL,
+	profileHash            CHAR(128)                   NOT NULL,
+	profileSalt            CHAR(64)                    NOT NULL,
+
+	-- make sure of elements that must be unique
+	UNIQUE (profileEmail),
+	UNIQUE (profileActivationToken),
+
+	-- declare primary key of profileId
+	PRIMARY KEY (profileId)
+);
+
 
 
 
