e60de9812bf99d9057b8c0851e316f45784c4ee9
baca.loren@gmail.com <baca.loren@gmail.com>
2016-08-05 10:53:22 -0600

    starting to code profile tables in SQL

--- sql/crumbtrail.sql
+++ sql/crumbtrail.sql
@@ -1,21 +1,27 @@
-/*  This is just a quick template.
+-- drop tables in reverse order to start fresh every time
 
-DROP TABLE IF EXISTS ***
+DROP TABLE IF EXISTS ???;
+DROP TABLE IF EXISTS ?????;
+DROP TABLE IF EXISTS ?????;
+DROP TABLE IF EXISTS ?????;
+DROP TABLE IF EXISTS ?????;
 
-CREATE TABLE *** (
-	*** INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	*** VARCHAR(128) NOT NULL,
-	PRIMARY KEY(**)
-));
 
-CREATE TABLE ***** (
-	-- This is a weak entity, which specifies the many-to-many
-	-- relationship between *** and ***.
-	******Id INT UNSIGNED NOT NULL,
-	****** INT UNSIGNED NOT NULL,
-	INDEX(******),
-	INDEX(******),
-	FOREIGN KEY(***) REFERENCES ***(****),
-	FOREIGN KEY(***) REFERENCES ***(****),
-	PRIMARY KEY(****, ****)
-);
+CREATE TABLE profile(
+	-- always start with your primary keys first!
+	-- make it of size and type INT, give it auto increment because it's a primary key, and require a value so use NOT NULL
+
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	-- now create other attributes within profileId
+	profileName VARCHAR(128) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL ,
+	profilePhone VARCHAR(32) NOT NULL ,
+
+	profileActiationToken VARCHAR(128), -- look into this one,  see if data type and initial Null value is ok. ask dylan if correct!!!*****
+
+	profileType VARCHAR()
+
+
+
+
+)
