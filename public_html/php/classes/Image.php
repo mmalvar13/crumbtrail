8eb53729196bb10dd906985d7b2b5b7285ab266c
Victoria Chacon <Vchaco06@gmail.com>
2016-08-06 16:00:26 -0600

    Added in the Docblock to image.php https://app.asana.com/0/161552024814178/164422210524646

---
+++ public_html/php/classes/Image.php
@@ -0,0 +1,20 @@
+<?php
+
+//namespace?
+//namespace //
+
+//autoload?//
+//require_once("autoload.php");//
+
+//Begin Docblock//
+
+/**
+ * Image that a company will provide
+ *
+ * This image will be what the company uses to identify themselves to the user. In future goals we are hoping to use this in order to give the companies a gallery to work with.
+ *
+ * @author Victoria Chacon <victoriousdesignco@gmail.com>
+**/
+//what does version apply to in the tweet example? is it necessary here?
+class Image
+//implements JsonSerializable?//
