37166bc75272f5ab4efd2e5d733226bd95473630
Monica Alvarez <mmalvar13@gmail.com>
2016-08-06 15:57:05 -0600

    added state variables and comment boxes for Event class https://app.asana.com/0/161555263951748/164422210524644

--- public_html/php/classes/Event.php
+++ public_html/php/classes/Event.php
@@ -1,7 +1,37 @@
 <?php
+namespace Edu\Cnm\Crumbtrail; //this is probably not right.
+
+require_once("autoload.php"); //idk
+
 /**
- * Created by PhpStorm.
- * User: mmalv
- * Date: 8/6/2016
- * Time: 3:29 PM
- */
+ * What do I explain here? Welcome to the Event class!
+ *
+ **/
+class Event{ //implement JsonSerializable??
+
+	/**
+	 * id for this event; this is the primary key
+	 * @var int $eventId
+	 **/
+	private $eventId;
+	/**
+	 * id of the Truck that attended this event; this is a foreign key
+	 * @var int $eventId
+	 **/
+	private $eventTruckId;
+	/**
+	 * end time for this event
+	 * @var \DateTime $eventEnd
+	 */
+	private $eventEnd;
+	/**
+	 * gps point location for this event
+	 * @geography point $eventLocation //is this right??
+	 */
+	private $eventLocation;
+	/**
+	 * start time for this event
+	 * @var \DateTime $eventStart
+	 **/
+	private $eventStart;
+}
