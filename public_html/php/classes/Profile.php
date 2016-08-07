9971431766ca080ded89504855c523dbdb616be2
baca.loren@gmail.com <baca.loren@gmail.com>
2016-08-06 20:39:41 -0600

    finished first draft of constructor for Profile class
    https://app.asana.com/0/161555263951748/164422210524636

--- public_html/php/classes/Profile.php
+++ public_html/php/classes/Profile.php
@@ -92,8 +92,19 @@ private $profileName;
 			$this->setProfileType($newProfileType);
 			$this->setProfileSalt($newProfileSalt);
 			$this->setProfileHash($newProfileHash);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			// rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			// rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			// rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			// rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
 		}
-
 
 	}
 
