7333c161d9b64af1ab9643a0c97bc076f1c6599e
Victoria Chacon <Vchaco06@gmail.com>
2016-08-07 13:59:17 -0600

    Added the exceptions in the constructor for Image.php https://app.asana.com/0/161552024814178/164422210524646

--- public_html/php/classes/Image.php
+++ public_html/php/classes/Image.php
@@ -45,8 +45,12 @@ class Image {
 	 * @param int $newImageCompanyId id of the Image that relates to a company
 	 * @param string $newImageFileType string containing the file type of an image
 	 * @param string $newImageFileName string containing the name of each image
-	 * @throws 
+	 * @throws \InvalidArgumentException if the data type is invalid
+	 * @throw \InvalidArgumentException if the id is negative
+	 * @throw \InvalidArgumentException if the image is not the correct file type
+	 * @throw \RangeException if the image name is longer than 255 characters
 	 */
+// WHAT WOULD I NEED TO DO IF I WANTED TO DEFINE FILE TYPE? //
 
 
 }
