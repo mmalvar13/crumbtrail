9705682b2006f8dedfdfe0a03867077cec1e0a60
Victoria Chacon <Vchaco06@gmail.com>
2016-08-08 15:55:58 -0600

    Added the docblock for Truck.php https://app.asana.com/0/161552024814178/164422210524642

---
+++ public_html/php/classes/Truck.php
@@ -0,0 +1,13 @@
+<?php
+
+//namespace
+//autoloader
+
+//begin Docblock
+/**
+ * Truck Identifier
+ * 
+ * This will be used to identify the different trucks that will be in our database. In future goals we would like this to expand past New Mexico.
+ * 
+ * @author Victoria Chacon <victoriousdesignco@gmail.com>
+ **/
