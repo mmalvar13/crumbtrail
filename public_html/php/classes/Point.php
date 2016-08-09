f4940d744d1df7c47a0602969d1d0d351987dd63
Monica Alvarez <mmalvar13@gmail.com>
2016-08-09 12:08:53 -0600

    added constructor for Point helper class https://app.asana.com/0/161555263951748/164422210524644

--- public_html/php/classes/Point.php
+++ public_html/php/classes/Point.php
@@ -18,9 +18,34 @@ class Point implements \JsonSerializable{
 	 * @var float $pointLongitude
 	 **/
 	private $pointLongitude;
-
-
-
 	}
+
+/**
+ * Constructor for this Point
+ * @param int $newPointLatitude the latitude for this point
+ * @param int $newPointLongitude the longitude
+ * @throws \RangeException if coordinate values are out of range
+ * @throws \TypeError if data types are incorrect
+ * @throws \Exception if any other exception occurs
+ **/
+public function __construct(float $newPointLatitude, float $newPointLongitude) {
+	try{
+		$this->setPointLatitude($newPointLatitude);
+		$this->setPointLongitude($newPointLongitude);
+	}catch(\RangeException $range){
+		//rethrow the exception to the caller
+		throw(new \RangeException($range->getMessage(), 0, $range));
+	}catch(\TypeError $typeError){
+		//rethrow the exception to the caller
+		throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+	}catch(\Exception $exception){
+		//rethrow the exception to the caller
+		throw(new \Exception($exception->getMessage(), 0, $exception));
+	}
+}
+
+
+
+
 
 }
