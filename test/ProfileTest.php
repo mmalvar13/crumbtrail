d91def9f7eacb3623f6446c0cee647dbd53f6a74
baca.loren@gmail.com <baca.loren@gmail.com>
2016-08-10 14:27:39 -0600

    added setup data for profile test
    https://app.asana.com/0/161555263951748/164422210524636

--- test/ProfileTest.php
+++ test/ProfileTest.php
@@ -29,16 +29,130 @@ class ProfileTest extends CrumbTrailTest {
 	/*----------------------------Declare Protected State Variables ----------------*/
 
 	/**
-	 * Default input data set for a string 1
-	 * @var string $VALID_STRING1     //why is this in all caps, what's up with this syntax?
+	 * Default input data set for name
+	 * @var string $VALID_PROFILENAME1     //why is this in all caps, what's up with this syntax?
 	 */
-	protected $VALID_STRING1 = "A man needs a name...";
+	protected $VALID_PROFILENAME1 = "Name";
 
 /**
- * default input data for a string 2
- * @var string  $VALID_STRING2
+ * default input data for updated name
+ * @var string  $VALID_PROFILENAME2
  */
-protected $VALID_STRING2 = "A girl has no name";
+protected $VALID_PROFILENAME2 = "NameUpdated";
+
+
+
+
+
+	/**
+ * Default input data set for email
+ * @var string $VALID_PROFILEEMAIL1
+ */
+	protected $VALID_PROFILEEMAIL1 = "email.cnm.edu";
+
+	/**
+	 * Default input data set for updated email
+	 * @var string $VALID_PROFILEEMAIL2
+	 */
+	protected $VALID_PROFILEEMAIL2 = "emailupdated.cnm.edu";
+
+
+
+
+
+	/**
+	 * Default input data set profile phone 10 chars
+	 * @var string $VALID_PROFILEPHONE1
+	 */
+	protected $VALID_PROFILEPHONE1 = "1111111111";
+
+	/**
+	 * Default input data set UPDATED profile phone 10 chars
+	 * @var string $VALID_PROFILEPHONE2
+	 */
+	protected $VALID_PROFILEPHONE2 = "2222222222";
+
+
+
+
+	/**
+	 * Default input data set profile access token 64 chars
+	 * @var string $VALID_PROFILEACCESSTOKEN1
+	 */
+	protected $VALID_PROFILEACCESSTOKEN1 = "0000000000000000000000000000000000000000000000000000000000004444";
+
+	/**
+	 * Default input data set UPDATED profile access token 64 chars
+	 * @var string $VALID_PROFILEACCESSTOKEN2
+	 */
+	protected $VALID_PROFILEACCESSTOKEN2 = "9999999999999999999999999999999999999999999999999999999999994444";
+
+
+
+
+
+	/**
+	 * Default input data set profile activation token 32 chars
+	 * @var string $VALID_PROFILEACTIVATIONTOKEN1
+	 */
+	protected $VALID_PROFILEACTIVATIONTOKEN1 = "00000000000000000000000000000022";
+
+	/**
+	 * Default input data set UPDATED profile activation token 32 chars
+	 * @var string $VALID_PROFILEACTIVATIONTOKEN2
+	 */
+	protected $VALID_PROFILEACTIVATIONTOKEN2 = "99999999999999999999999999999922";
+
+
+
+
+
+	/**
+	 * Default input data set profile type
+	 * @var string $VALID_PROFILETYPE1
+	 */
+	protected $VALID_PROFILETYPE1 = "a";
+
+	/**
+	 * Default input data set UPDATED profile type
+	 * @var string $VALID_PROFILETYPE2
+	 */
+	protected $VALID_PROFILETYPE2 = "o";
+
+
+
+
+
+	/**
+	 * Default input data set profile hash 128 chars
+	 * @var string $VALID_PROFILEHASH1
+	 */
+	protected $VALID_PROFILEHASH1 = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000088888888";
+
+	/**
+	 * Default input data set UPDATED profile hash 128 chars
+	 * @var string $VALID_PROFILEHASH2
+	 */
+	protected $VALID_PROFILEHASH2 = "99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999988888888";
+
+
+
+
+
+	/**
+ * Default input data set profile salt 64 chars
+ * @var string $VALID_PROFILESALT1
+ */
+	protected $VALID_PROFILESALT1 = "0000000000000000000000000000000000000000000000000000000000004444";
+
+	/**
+	 * Default input data set UPDATED profile salt 64 chars
+	 * @var string $VALID_PROFILESALT2
+	 */
+	protected $VALID_PROFILESALT2 = "9999999999999999999999999999999999999999999999999999999999994444";
+
+
+
 
 	//AM I CORRECT IN ASSUMING i DONT NEED ANYTHING FOR THE PRIMARY KEY, AND i DONT HAVE ANY FOREIGN KEYS, SO NOTHING FOR THAT
 
@@ -68,6 +182,7 @@ protected $VALID_STRING2 = "A girl has no name";
 		$numRows = $this->getConnection()->getRowCount("profile");  // so..get connection to SQL, and get the count of rows for a particular profile??
 
 		//create a new profile and insert it into SQL
+		$profile = new Profile(null, $this->profile->getProfileId())
 
 	}
 	}
