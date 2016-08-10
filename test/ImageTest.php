e9b9c0f0780b494feab35db9019ff25e9f6848d3
Victoria Chacon <Vchaco06@gmail.com>
2016-08-10 15:59:13 -0600

    Wrote the setup method for the imageTest https://app.asana.com/0/161552024814178/164422210524646

--- test/ImageTest.php
+++ test/ImageTest.php
@@ -1,7 +1,7 @@
 <?php
 namespace Edu\Cnm\Crumbtrail\Test;
 
-use Edu\Cnm\Crumbtrail\{Image};
+use Edu\Cnm\Crumbtrail\{Image, Company};
 
 //grab the project test parameters
 require_once("CrumbTrailTest.php");
@@ -40,6 +40,21 @@ class ImageTest extends CrumbTrailTest {
 	 **/
 	protected $VALID_IMAGEFILENAME2 = "SomeAwesomeUnderstatedCuisineEatery";
 	/**
+	/**
 	 * create dependent objects before running each test
 	 **/
+	//run the default setUp() method first (creating a fake company to house the test image)
+	public final function setUp() {
+		//run the default setUp() method first
+		parent::setUp();
+
+		//create and insert a Company to own the test image
+		$this->company = new Company(null, "Terry's Tacos", "terrytacos@tacos.com", "12345", "2345", "Terry Jane", "345 Taco Street", "Albuquerque", "NM", "87654" "We are a Taco truck description", "Tacos, Tortillas, Burritos" "5052345678","1");
+		$this->company->insert($this->getPDO());
+	}
+	/**
+	 * insert valid image and verify that the actual mySQL data matches
+	 **/ 
+	public function testInsertValidImage() {}
+	
 }
