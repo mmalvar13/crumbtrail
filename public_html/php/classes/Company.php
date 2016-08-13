54dbffe7bdb801b6c1e3e0508c348d8a8aac41ce
Victoria Chacon <Vchaco06@gmail.com>
2016-08-13 13:47:27 -0600

    Fixed line 539 in the Company.php made camel case and added $ instead of #

--- public_html/php/classes/Company.php
+++ public_html/php/classes/Company.php
@@ -534,7 +534,8 @@ class Company implements \JsonSerializable {
 		return ($this->companyZip);
 	}
 
-	/**  Mutator method (setter) for companyZip.
+	/**
+	 * Mutator method (setter) for companyZip.
 	 * @param int|null $newCompanyZip The new value of companyZip.
 	 * @throws \RangeException  if $newCompanyZip is not a positive.
 	 * @throws \TypeError if $newCompanyZip is not an integer.
