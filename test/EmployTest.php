a20d3393c721498108df48ea5bba26326a21d821
Monica Alvarez <mmalvar13@gmail.com>
2016-08-13 10:53:17 -0600

    fixed require once in employ test https://app.asana.com/0/161555263951748/164422210524640

--- test/EmployTest.php
+++ test/EmployTest.php
@@ -7,7 +7,7 @@ use Edu\Cnm\CrumbTrail\{Profile, Company, Employ};
 require_once("CrumbTrailTest.php");
 
 //grab the class under scrutiny
-require_once(dirname(__DIR__) . "public_html/php/classes/autoload.php");
+require_once(dirname(__DIR__) . "/public_html/php/classes/autoload.php");
 
 /**
  * Full PHPUnit test for the Employ class
