7a52ee5f5f9155cfbdbfc6a4795cdc945bbc74f3
baca.loren@gmail.com <baca.loren@gmail.com>
2016-08-13 13:43:39 -0600

    fixed getProfileByProfilePhone
    https://app.asana.com/0/161555263951748/164422210524636

--- public_html/php/classes/Profile.php
+++ public_html/php/classes/Profile.php
@@ -241,7 +241,7 @@ public static function getProfileByProfileId(\PDO $pdo, int $profileId){
 		}
 
 		if(strlen($profileEmail)>128){
-			throw(new \PDOException("The profile name entered is too long"));
+			throw(new \PDOException("The profile Phone entered is too long"));
 		}
 
 		//create query template
@@ -276,51 +276,59 @@ public static function getProfileByProfileId(\PDO $pdo, int $profileId){
 	}
 
 
+
 	/**
 	 * gets profile by the profile phone
 	 * @param \PDO $pdo PDO connection object
 	 * @param string $profilePhone used as the profile phone to search for
-	 * @return Profile|null Profile found or null if not found
+	 * @return \SplFixedArray SplFixedArray of profiles found
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when variables are not the correct data type
 	 */
 	public static function getProfileByProfilePhone(\PDO $pdo, string $profilePhone){
 		//sanitize the ID before searching for it
-		//do we want to enforce a 10 digit phone number
-		if(strlen($profilePhone) < 7){
-			throw(new \PDOException("The profile phone number is too short"));
+		$profilePhone = trim($profilePhone);
+		$profilePhone = filter_var($profilePhone, FILTER_SANITIZE_STRING);
+
+		if(empty($profilePhone) === true){
+			throw(new \PDOException("The profile email is empty"));
+		}
+
+		if(strlen($profilePhone)>32){
+			throw(new \PDOException("The profile Phone entered is too long"));
 		}
 
 		//create query template
-		$query = "SELECT profileId, profileName, profileEmail, profilePhone, profileAccessToken, profileActivationToken, profileType, profileHash, profileSalt FROM profile WHERE profilePhone = :profilePhone";
+		$query = "SELECT profileId, profileName, profileEmail, profilePhone, profileAccessToken, profileActivationToken, profileType, profileHash, profileSalt FROM profile WHERE profilePhone LIKE :profilePhone";
 
 		//prepare template
 		$statement = $pdo->prepare($query);
 
-		//bind the profilePhone to the placeholder in the template ***WHY JUST TO PROFILE phone?***
+		//bind the profileId to the placeholder in the template
+		$profilePhone = "%$profilePhone%";
 		$parameters = ["profilePhone"=>$profilePhone];
 		//execute the SQL statement
 		$statement->execute($parameters);
 
-		//now that we have selected the correct profile, we need to grab it from mySQL
-		try{
-			$profile = null;   //new variable $profile is what we will assign all the information in this profile to, and return to whatever called this method
-			$statement->setFetchMode(\PDO::FETCH_ASSOC); //what is going on here? establishing the fetch mode for this method?
-			//$row is an empty array I think
-			$row = $statement->fetch(); //create new variable and assign it to $statement which is pointing to the value of fetch???
+		//build an array of profiles
+		$profiles = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
 
-			//what does it mean for the row to be false?? That the row is empty? Couldnt be retrieved???
-			if($row !== false){
-				//set $profile to a new object based on the Profile class with these values assigned into it (I think)
+		while(($row = $statement->fetch()) !== false){
+			try{
 				$profile = new Profile($row["profileId"], $row["profileName"], $row["profileEmail"], $row["profilePhone"], $row["profileAccessToken"], $row["profileActivationToken"], $row["profileType"], $row["profileHash"], $row["profileSalt"]);
+
+				//What exactly is happening here ****
+				$profiles[$profiles->key()] = $profile;
+				$profiles->next();
+			}catch(\Exception $exception){
+				//if the row couldn't be converted, rethrow it (the error?)
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
-			//catch statement
-		} catch(\Exception $exception){
-			// if the row couldnt be converted, re-throw it
-			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
-		return($profile);
+		return($profiles);
 	}
+
 
 
 
