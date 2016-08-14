4d8948b64aaf42ec8eb8a1568e520fe151c520c6
Victoria Chacon <Vchaco06@gmail.com>
2016-08-14 10:04:52 -0600

    deleted get truckbytruckcontent.
    https://app.asana.com/0/161552024814178/164422210524646

--- test/TruckTest.php
+++ test/TruckTest.php
@@ -145,35 +145,6 @@ class TruckTest extends CrumbTrailTest {
 		$truck->delete($this->getPDO());
 	}
 	/**
-	 * test grabbing a Truck by truck content?
-	 */
-	public function testGetValidTruckByTruckContent() {
-		//count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("truck");
-
-		//create a new Truck and insert it into mySQL
-		$truck = new Truck(null, $this->company->getCompanyId());
-		$truck->insert($this->getPDO());
-
-		//grab the data from mySQL and enforce the fields match our expectations
-		$results = Truck::getTruckByTruckId($this->getPDO(), $truck->getTruckId());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("truck"));
-		$this->assertCount(1, $results);
-		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\CrumbTrail\\Truck", $results);
-
-		//grab the result from the array and validate it
-		$pdoTruck = $results[0];
-		$this->assertEquals($pdoTruck->getCompanyId(), $this->company->getCompanyId());
-	}
-	/**
-	 * test grabbing a Truck by content that does not exist
-	 **/
-	public function testGetInvalidTruckByTruckContent() {
-		//grab an image by searching for content that does not exist?
-		$truck = Truck::getTruckByTruckId($this->getPDO(), "No truck information found");
-		$this->assertCount(0, $truck);
-	}
-	/**
 	 * test grabbing all trucks
 	 **/
 	public function testGetAllValidTrucks() {
