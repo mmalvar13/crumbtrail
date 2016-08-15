ca504dd9a9794ab9ec5e99915f6a615e6ac32ef4
Victoria Chacon <Vchaco06@gmail.com>
2016-08-15 17:05:43 -0600

    changed image file type in the image class
    https://app.asana.com/0/161552024814178/164422210524646

--- public_html/php/classes/Image.php
+++ public_html/php/classes/Image.php
@@ -281,7 +281,7 @@ class Image implements \JsonSerializable {
 		$validFileType = ["image/jpeg", "image/jpg", "image/png"];
 		$newImageFileType = strtolower($newImageFileType);
 		if(in_array($newImageFileType, $validFileType) === false) {
-			throw(new \PDOException("This is not the proper image type. Please insert jpeg, or png"));
+			throw(new \PDOException("This is not the proper image type. Please insert image/jpeg, or image/png"));
 		}
 		//convert and store
 		$this->imageFileType = $newImageFileType;
