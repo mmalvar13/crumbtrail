a5cf89a231377d70ee16f4204f50ebb1f98ded04
Kevin Lee Kirk <klkirkhome@gmail.com>
2016-08-15 17:22:21 -0600

    Added expectedException , to PointTest. https://app.asana.com/0/161555263951748/164422210524638

--- test/PointTest.php
+++ test/PointTest.php
@@ -67,6 +67,8 @@ class PointTest extends \PHPUnit_Framework_TestCase {
 	 * We expect an exception because the data values are out of bounds,
 	 * (e.g., latitude > 90 or < -90, or  longitude > 180 or < -180).
 	 *
+	 *  @expectedException \RangeException if lat and long are bad.
+	 *
 	 */
 	public function testInvalidPoint() {
 
