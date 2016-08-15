3d342bbec151de5439727450fe1e728308aa8fd1
Victoria Chacon <Vchaco06@gmail.com>
2016-08-15 09:37:53 -0600

    Took out getTruckByTruckContent in Truck test
    https://app.asana.com/0/161552024814178/164422210524642

--- test/TruckTest.php
+++ test/TruckTest.php
@@ -182,4 +182,4 @@ class TruckTest extends CrumbTrailTest {
 		$this->assertEquals($pdoTruck->getCompanyId(), $this->company->getCompanyId());
 		}
 	}
-	//This is the end of the Truck unit test, trial one//
+	//This is the end of the Truck unit test, trial two//
