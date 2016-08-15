6d02d6e7111769ff0c859fa15ef18234060fe1d7
Monica Alvarez <mmalvar13@gmail.com>
2016-08-14 22:20:24 -0600

    changed lines 214 and 215 to getProfileId and getCompanyId https://app.asana.com/0/161555263951748/164422210524640

--- test/EmployTest.php
+++ test/EmployTest.php
@@ -211,8 +211,8 @@ class EmployTest extends CrumbTrailTest {
 
 		//grab the result from the array and validate it
 		$pdoEmploy = $results[0];
-		$this->assertEquals($pdoEmploy->getEmployProfileId(), $this->profile->getProfileId());
-		$this->assertEquals($pdoEmploy->getEmployCompanyId(), $this->company->getCompanyId());
+		$this->assertEquals($pdoEmploy->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoEmploy->getCompanyId(), $this->company->getCompanyId());
 
 	}
 
