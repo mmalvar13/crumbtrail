558067ac0dc55534d94cbfc03136eb4eec60cd2b
Kevin Lee Kirk <klkirkhome@gmail.com>
2016-08-15 09:13:08 -0600

    companyApproved re-formated, in Company.php https://app.asana.com/0/161555263951748/164422210524638

--- public_html/php/classes/Company.php
+++ public_html/php/classes/Company.php
@@ -996,10 +996,17 @@ class Company implements \JsonSerializable {
 		// Prepare this query.
 		$statement = $pdo->prepare($query);
 
+		// change the format of the boolean companyApproved
+		if($this->companyApproved === false) {
+			$formatCompanyApproved = 0;
+		} else {
+			$formatCompanyApproved = $this->companyApproved;
+		}
+
 		// Bind the variables to the template.
 
 		//taking "companyAccountCreatorId" => $this->companyAccountCreatorId out of array for update
-		$parameters = ["companyAccountCreatorId" => $this->companyAccountCreatorId, "companyName" => $this->companyName, "companyEmail" => $this->companyEmail, "companyPhone" => $this->companyPhone, "companyPermit" => $this->companyPermit, "companyLicense" => $this->companyLicense, "companyAttn" => $this->companyAttn, "companyStreet1" => $this->companyStreet1, "companyStreet2" => $this->companyStreet2, "companyCity" => $this->companyCity, "companyState" => $this->companyState, "companyZip" => $this->companyZip, "companyDescription" => $this->companyDescription, "companyMenuText" => $this->companyMenuText, "companyActivationToken" => $this->companyActivationToken, "companyApproved" => $this->companyApproved, "companyId" => $this->companyId];
+		$parameters = ["companyAccountCreatorId" => $this->companyAccountCreatorId, "companyName" => $this->companyName, "companyEmail" => $this->companyEmail, "companyPhone" => $this->companyPhone, "companyPermit" => $this->companyPermit, "companyLicense" => $this->companyLicense, "companyAttn" => $this->companyAttn, "companyStreet1" => $this->companyStreet1, "companyStreet2" => $this->companyStreet2, "companyCity" => $this->companyCity, "companyState" => $this->companyState, "companyZip" => $this->companyZip, "companyDescription" => $this->companyDescription, "companyMenuText" => $this->companyMenuText, "companyActivationToken" => $this->companyActivationToken, "companyApproved" => $formatCompanyApproved, "companyId" => $this->companyId];
 
 		// Execute the mySQL statement.
 		$statement->execute($parameters);
