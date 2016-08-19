890be1bf3e7671a7e0bf41ea20bd35a5290d4b93
baca.loren@gmail.com <baca.loren@gmail.com>
2016-08-18 21:27:10 -0600

    Started putting together company api
    https://app.asana.com/0/161555263951748/169698496837359

---
+++ public_html/php/apis/company.php
@@ -0,0 +1,29 @@
+<?php
+
+
+//We need to:
+//get all companies
+//get a specific company by primary key
+//create a new company
+//update a company by primary key
+//delete a company by primary key
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once("/etc/apache2/crumbtrail-mysql/encrypted-config.php");
+
+/**
+ * api for the Company class
+ *
+ * @author Loren Baca baca.loren@gmail.com
+ */
+
+//verify the session, start one if not active
+if(session_status() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
