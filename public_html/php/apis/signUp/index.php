78b566d29378787dd4410e48d8c51676615bc03c
Monica Alvarez <mmalvar13@gmail.com>
2016-08-20 15:43:00 -0600

    changed require ocne to capstone-mysql instead of crumbtrail-mysql https://app.asana.com/0/161555263951748/169698496837351

--- public_html/php/apis/signUp/index.php
+++ public_html/php/apis/signUp/index.php
@@ -2,7 +2,7 @@
 
 require_once "autoloader.php";//where is this? same autoload.php as before or a new one?
 require_once "/lib/xsrf.php"; //when do we make this?
-require_once("/etc/apache2/crumbtrail-mysql/encrypted-config.php"); //do i put crumbtrail-mysql here?
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php"); //do i put crumbtrail-mysql here?
 
 use Edu\Cnm\Crumbtrail\{Company, Profile}; //is this correct? i dont have to add mmalvar13 right? do i add company and profile like this?
 
