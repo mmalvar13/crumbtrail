5cc2a21d7760e7404865108c5b25fad37b797845
Monica Alvarez <mmalvar13@gmail.com>
2016-08-20 16:18:01 -0600

    added comments and added exceptions to insist on unique business license https://app.asana.com/0/161555263951748/169698496837351

--- public_html/php/apis/signUp/index.php
+++ public_html/php/apis/signUp/index.php
@@ -42,18 +42,24 @@ try {
 		$requestContent = file_get_contents("php://input"); //what is the directory reference here??
 		$requestObject = json_decode($requestContent);
 
-
-		//somewhere here do i also have to ensure that company id is empty when making a new c Company? yes i think so.
-		//make sure profileId is empty because we are creating a new profile. or is this where we throw exceptions for email and stuff?
+		//make sure profileId is empty because we are creating a new profile
+		//is this where we throw exceptions for email and stuff?
 		if(empty($requestObject->profileId) === false) {
 			throw(new \InvalidArgumentException("This profile already exists", 405));
 		}
+		//make sure comapnyId is empty because we are signing up a new company
 		if(empty($requestObject->companyId) === false) {
 			throw(new \InvalidArgumentException("This company already exists", 405));
 		}
+		//make sure profileEmail is unique
 		if($requestObject->profileEmail === ){
 			throw(new \InvalidArgumentException("This email is already attached to another profile", 405)); //what error code would i throw. what do i compare the inserted profile email too? do I have to traverse an array of getAllEmails???
 		}
+		//make sure comapny business license is unique
+		if($requestObject->companyLicense ===){
+			throw(new \InvalidArgumentException("A company already exists under this business license", 405));
+		}
+
 		//what other exceptions do i throw here? what am i trying to catch here? and what do i try to catch in angular??
 
 		//create a new profile and insert it into the databases
@@ -61,12 +67,14 @@ try {
 
 		$profile->insert($pdo);
 
-		$company = new Company(null, $requestObject->companyAccountCreatorId, $requestObject->companyName, $requestObject->companyEmail, $requestObject->companyPhone, $requestObject->companyPermit, $requestObject->companyLicense, $requestObject->companyAttn, $requestObject->companyStreet1, $requestObject->companyStreet2, $requestObject->companyCity, $requestObject->companyState, $requestObject->companyZip, $requestObject->companyActivationToken, $requestObject->companyApproved); //took out companyMenuText and companyDescription. will be inserted in company api. right?
+		//here i took out companyDescription and companyMenuText. this will be inserted in the Company API, correct?
+		$company = new Company(null, $requestObject->companyAccountCreatorId, $requestObject->companyName, $requestObject->companyEmail, $requestObject->companyPhone, $requestObject->companyPermit, $requestObject->companyLicense, $requestObject->companyAttn, $requestObject->companyStreet1, $requestObject->companyStreet2, $requestObject->companyCity, $requestObject->companyState, $requestObject->companyZip, $requestObject->companyActivationToken, $requestObject->companyApproved);
 
 		$company->insert($pdo);
 
 		//update reply
 		$reply->message = "Account created OK";
+
 	} else {
 		throw(new InvalidArgumentException("Invalid HTTP method request"));
 	}
