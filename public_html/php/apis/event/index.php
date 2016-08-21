a2096a3d9e1e1883ccb5d9394101777430afc199
Victoria Chacon <Vchaco06@gmail.com>
2016-08-21 16:46:04 -0600

    Added in all the attributes in the Event class to the if/else/elseif block
    https://app.asana.com/0/161552024814178/169698496837363

--- public_html/php/apis/event/index.php
+++ public_html/php/apis/event/index.php
@@ -15,7 +15,7 @@ use Edu\Cnm\CrumbTrail\ {
 
 require_once "autoloader.php";
 require_once "/lib/xsrf.php";
-require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 /**
  * api for the Event class
@@ -45,6 +45,50 @@ try {
 	//sanitize input
 	//using the Get method here right? Would i need this for event Id??
 	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	//make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
 
+	//handle GET request - if id is present, that event is returned, otherwise all Events are returned
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+
+		//get a specific event or all events and update reply
+		if(empty($id) === false) {
+			$event = Event::getEventByEventId($pdo, $id);
+			if($event !== null) {
+				$reply->data = $event;
+			}
+		} elseif((empty($id)) === false) {
+			$event = Event::getEventByEventTruckId($pdo, $id);
+			if($event !== null) {
+				$reply->data = $event;
+			}
+		} elseif((empty($id)) === false) {
+			$event = Event::getEventByEventLocation($pdo, $id);
+			if($event !== null) {
+				$reply->data = $event;
+			}
+		} elseif((empty($id)) === false) {
+			$event = Event::getEventByEventIdAndEventTruckId($pdo, $id);
+			if($event !== null) {
+				$reply->data = $event;
+			}
+		} elseif((empty($id)) === false) {
+			$event = Event::getEventByEventEndAndEventStart($pdo, $id);
+			if($event !== null) {
+				$reply->data = $event;
+			}
+		} else {
+			$events = Event::getAllEvents($pdo);
+			if($events !== null) {
+				$reply->data = $events;
+			}
+		}
+	} elseif ((method === "PUT" || "POST")) {
+	}
 }
+
 
