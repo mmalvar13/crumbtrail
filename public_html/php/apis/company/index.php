cd2c662de672f7bed87006e67f243db2e706cf7c
baca.loren@gmail.com <baca.loren@gmail.com>
2016-08-22 16:17:18 -0600

    added a wrapper for the PUT/POST/DELETE to ensure the person makeing changes own that profile, and is either an ADMIN or OWNER
    https://app.asana.com/0/161555263951748/169698496837359

--- public_html/php/apis/company/index.php
+++ public_html/php/apis/company/index.php
@@ -16,7 +16,9 @@
 //DELETE a company by the primary key
 
 
-use Edu\Cnm\CrumbTrail\{Profile, Company};
+use Edu\Cnm\CrumbTrail\{
+	Profile, Company
+};
 
 
 require_once "autoloader.php";
@@ -70,7 +72,6 @@ try {
 		setXsrfCookie();
 
 
-
 		//get a specific company or all companies and update reply
 		if((empty($id)) === false) {
 			$company = Company::getCompanyByCompanyId($pdo, $id);
@@ -78,7 +79,7 @@ try {
 				$reply->data = $company;
 			}
 		} elseif((empty($id)) === false) {
-			$company = Company::getCompanyByCompanyAccountCreatorId($pdo,$companyAccountCreatorId);
+			$company = Company::getCompanyByCompanyAccountCreatorId($pdo, $companyAccountCreatorId);
 			if($company !== null) {
 				$reply->data = $company;
 			}
@@ -108,179 +109,177 @@ try {
 		//HOW DO I PUT A CATCH HERE TO MAKE SURE ONLY A ADMIN OR OWNER IS MAKING A PUT OR POST????
 		//maybe I do still need a post in case they fill out a form later on? That would only work if we didnt make all
 		//fields required on signup.
-	} elseif(($method === "PUT" || "POST")) {
-		verifyXsrf();
-		$requestContent = file_get_contents("php://input");
-		$requestObject = json_decode($requestContent);
+		//put all this in a check to the session, where we check on the profile type(a/o) and also check to ensure the profile ID, matches
+		//the profile they are trying to PUT/POST
 
-		//ensure that the profile trying to make changes to company is either admin or owner
+	} elseif((empty($_SESSION["profile"]) === false) && (($_SESSION["profile"]->getProfileId()) === $id) && (($_SESSION["profile"]->getProfileType()) === "a") || (($_SESSION["profile"]->getProfileType())) === "o") {
 
+		if(($method === "PUT" || $method === "POST")) {
+			verifyXsrf();
+			$requestContent = file_get_contents("php://input");
+			$requestObject = json_decode($requestContent);
 
+			//ensure that the profile trying to make changes to company is either admin or owner
 
-		//***SHIIIIIIII MAKE SURE TO ADD IN A CHECK TO SEE IF THE PERSON MAKING CHANGES ACTUALLY OWNS THAT ACCOUNT!!!******
-
-
-
-		$profile = Profile::getProfileByProfileId($pdo, $id);
-		if($requestObject->profileType !== 'a' || 'o') {
-			throw(new \InvalidArgumentException("profile type must be admin('a') or owner('o') in order to make changes to a profile"));
-		}
+			//***SHIIIIIIII MAKE SURE TO ADD IN A CHECK TO SEE IF THE PERSON MAKING CHANGES ACTUALLY OWNS THAT ACCOUNT!!!******
 
 
-		//make sure the company name is available (required field)
-		if(empty($requestObject->companyName) === true) {
-			throw(new \InvalidArgumentException("No name for company", 405));
-		}
-
-		//make sure the company email is available (required field)
-		if(empty($requestObject->companyEmail) === true) {
-			throw(new \InvalidArgumentException("No email for company", 405));
-		}
-
-		//make sure the company phone is available (required field)
-		if(empty($requestObject->companyPhone) === true) {
-			throw(new \InvalidArgumentException("No phone for company", 405));
-		}
-
-		//make sure the company permit is available (required field)
-		if(empty($requestObject->companyPermit) === true) {
-			throw(new \InvalidArgumentException("No permit for company", 405));
-		}
-
-		//make sure the company license is available (required field)
-		if(empty($requestObject->companyLicense) === true) {
-			throw(new \InvalidArgumentException("No license for company", 405));
-		}
-
-		//make sure the company Attn is available (required field)
-		if(empty($requestObject->companyAttn) === true) {
-			throw(new \InvalidArgumentException("No Attn for company", 405));
-		}
-
-		//make sure the company street1 is available (required field)
-		if(empty($requestObject->companyStreet1) === true) {
-			throw(new \InvalidArgumentException("No street1 for company", 405));
-		}
-
-		//make sure the company street2 is available (required field)
-		if(empty($requestObject->companyStreet2) === true) {
-			throw(new \InvalidArgumentException("No street2 for company", 405));
-		}
-
-		//make sure the company city is available (required field)
-		if(empty($requestObject->companyCity) === true) {
-			throw(new \InvalidArgumentException("No city for company", 405));
-		}
-
-		//make sure the company state is available (required field)
-		if(empty($requestObject->companyState) === true) {
-			throw(new \InvalidArgumentException("No state for company", 405));
-		}
-
-		//make sure the company zip is available (required field)
-		if(empty($requestObject->companyZip) === true) {
-			throw(new \InvalidArgumentException("No zip for company", 405));
-		}
-
-		//make sure the company description is available (required field)
-		if(empty($requestObject->companyDescription) === true) {
-			throw(new \InvalidArgumentException("No description for company", 405));
-		}
-
-		//make sure the company menu text is available (required field)
-		if(empty($requestObject->companyMenuText) === true) {
-			throw(new \InvalidArgumentException("No menu for company", 405));
-		}
-
-		//make sure the company account creator ID is available (required field)
-		if(empty($requestObject->companyAccountCreatorId) === true) {
-			throw(new \InvalidArgumentException("No company account creator ID for company", 405));
-		}
-
-		//perform the actual PUT or POST
-
-		//I will need 2 sections of PUT, one for ADMINS & Owners, and 1 for JUST ADMINS to change things like permits/license etc
-		if($method === "PUT" && $requestObject->companyType === 'a') {
-			//retrieve the company to update
-
-			$company = Company::getCompanyByCompanyId($pdo, $id);
-			if($company === null) {
-				throw(new RuntimeException("This company does not exist", 404));
+			//make sure the company name is available (required field)
+			if(empty($requestObject->companyName) === true) {
+				throw(new \InvalidArgumentException("No name for company", 405));
 			}
-			//put the new company name into the company and update
-			$company->setCompanyName($requestObject->companyName);
-			$company->setCompanyEmail($requestObject->companyEmail);
-			$company->setCompanyPhone($requestObject->companyPhone);
-			$company->setCompanyName($requestObject->companyName);
-			$company->setCompanyPermit($requestObject->companyPermit);
-			$company->setCompanyLicense($requestObject->companyLicense);
-			$company->setCompanyAttn($requestObject->companyAttn);
-			$company->setCompanyStreet1($requestObject->companyStreet1);
-			$company->setCompanyStreet2($requestObject->companyStreet2);
-			$company->setCompanyCity($requestObject->companyCity);
-			$company->setCompanyState($requestObject->companyState);
-			$company->setCompanyZip($requestObject->companyZip);
-			$company->setCompanyDescription($requestObject->companyDescription);
-			$company->setCompanyMenuText($requestObject->companyMenuText);
-			$company->setCompanyName($requestObject->companyName);
 
-			$company->update($pdo);
-
-			//update reply
-			$reply->message = "Company updated A-OK";
-
-		} elseif($method === "PUT" && $requestObject->companyType === 'o') {
-			//retrieve the company to update
-
-			$company = Company::getCompanyByCompanyId($pdo, $id);
-			if($company === null) {
-				throw(new RuntimeException("This company does not exist", 404));
+			//make sure the company email is available (required field)
+			if(empty($requestObject->companyEmail) === true) {
+				throw(new \InvalidArgumentException("No email for company", 405));
 			}
-			//put the new company name into the company and update
-			$company->setCompanyName($requestObject->companyName);
-			$company->setCompanyEmail($requestObject->companyEmail);
-			$company->setCompanyPhone($requestObject->companyPhone);
-			$company->setCompanyName($requestObject->companyName);
-			//what else needs to be omitted from owner change
+
+			//make sure the company phone is available (required field)
+			if(empty($requestObject->companyPhone) === true) {
+				throw(new \InvalidArgumentException("No phone for company", 405));
+			}
+
+			//make sure the company permit is available (required field)
+			if(empty($requestObject->companyPermit) === true) {
+				throw(new \InvalidArgumentException("No permit for company", 405));
+			}
+
+			//make sure the company license is available (required field)
+			if(empty($requestObject->companyLicense) === true) {
+				throw(new \InvalidArgumentException("No license for company", 405));
+			}
+
+			//make sure the company Attn is available (required field)
+			if(empty($requestObject->companyAttn) === true) {
+				throw(new \InvalidArgumentException("No Attn for company", 405));
+			}
+
+			//make sure the company street1 is available (required field)
+			if(empty($requestObject->companyStreet1) === true) {
+				throw(new \InvalidArgumentException("No street1 for company", 405));
+			}
+
+			//make sure the company street2 is available (required field)
+			if(empty($requestObject->companyStreet2) === true) {
+				throw(new \InvalidArgumentException("No street2 for company", 405));
+			}
+
+			//make sure the company city is available (required field)
+			if(empty($requestObject->companyCity) === true) {
+				throw(new \InvalidArgumentException("No city for company", 405));
+			}
+
+			//make sure the company state is available (required field)
+			if(empty($requestObject->companyState) === true) {
+				throw(new \InvalidArgumentException("No state for company", 405));
+			}
+
+			//make sure the company zip is available (required field)
+			if(empty($requestObject->companyZip) === true) {
+				throw(new \InvalidArgumentException("No zip for company", 405));
+			}
+
+			//make sure the company description is available (required field)
+			if(empty($requestObject->companyDescription) === true) {
+				throw(new \InvalidArgumentException("No description for company", 405));
+			}
+
+			//make sure the company menu text is available (required field)
+			if(empty($requestObject->companyMenuText) === true) {
+				throw(new \InvalidArgumentException("No menu for company", 405));
+			}
+
+			//make sure the company account creator ID is available (required field)
+			if(empty($requestObject->companyAccountCreatorId) === true) {
+				throw(new \InvalidArgumentException("No company account creator ID for company", 405));
+			}
+
+			//perform the actual PUT or POST
+
+			//I will need 2 sections of PUT, one for ADMINS & Owners, and 1 for JUST ADMINS to change things like permits/license etc
+			if($method === "PUT") {
+				//retrieve the company to update
+
+				$company = Company::getCompanyByCompanyId($pdo, $id);
+				if($company === null) {
+					throw(new RuntimeException("This company does not exist", 404));
+				}
+				//put the new company name into the company and update
+				$company->setCompanyName($requestObject->companyName);
+				$company->setCompanyEmail($requestObject->companyEmail);
+				$company->setCompanyPhone($requestObject->companyPhone);
+				$company->setCompanyName($requestObject->companyName);
+				$company->setCompanyPermit($requestObject->companyPermit);
+				$company->setCompanyLicense($requestObject->companyLicense);
+				$company->setCompanyAttn($requestObject->companyAttn);
+				$company->setCompanyStreet1($requestObject->companyStreet1);
+				$company->setCompanyStreet2($requestObject->companyStreet2);
+				$company->setCompanyCity($requestObject->companyCity);
+				$company->setCompanyState($requestObject->companyState);
+				$company->setCompanyZip($requestObject->companyZip);
+				$company->setCompanyDescription($requestObject->companyDescription);
+				$company->setCompanyMenuText($requestObject->companyMenuText);
+				$company->setCompanyName($requestObject->companyName);
+
+				$company->update($pdo);
+
+				//update reply
+				$reply->message = "Company updated A-OK";
+
+			} elseif($method === "PUT") {
+				//retrieve the company to update
+
+				$company = Company::getCompanyByCompanyId($pdo, $id);
+				if($company === null) {
+					throw(new RuntimeException("This company does not exist", 404));
+				}
+				//put the new company name into the company and update
+				$company->setCompanyName($requestObject->companyName);
+				$company->setCompanyEmail($requestObject->companyEmail);
+				$company->setCompanyPhone($requestObject->companyPhone);
+				$company->setCompanyName($requestObject->companyName);
+				//what else needs to be omitted from owner change
 //			$company->setCompanyPermit($requestObject->companyPermit);
 //			$company->setCompanyLicense($requestObject->companyLicense);
-			$company->setCompanyAttn($requestObject->companyAttn);
-			$company->setCompanyStreet1($requestObject->companyStreet1);
-			$company->setCompanyStreet2($requestObject->companyStreet2);
-			$company->setCompanyCity($requestObject->companyCity);
-			$company->setCompanyState($requestObject->companyState);
-			$company->setCompanyZip($requestObject->companyZip);
-			$company->setCompanyDescription($requestObject->companyDescription);
-			$company->setCompanyMenuText($requestObject->companyMenuText);
-			$company->setCompanyName($requestObject->companyName);
+				$company->setCompanyAttn($requestObject->companyAttn);
+				$company->setCompanyStreet1($requestObject->companyStreet1);
+				$company->setCompanyStreet2($requestObject->companyStreet2);
+				$company->setCompanyCity($requestObject->companyCity);
+				$company->setCompanyState($requestObject->companyState);
+				$company->setCompanyZip($requestObject->companyZip);
+				$company->setCompanyDescription($requestObject->companyDescription);
+				$company->setCompanyMenuText($requestObject->companyMenuText);
+				$company->setCompanyName($requestObject->companyName);
 
-			$company->update($pdo);
+				$company->update($pdo);
 
-			//update reply
-			$reply->message = "Company updated A-OK";
+				//update reply
+				$reply->message = "Company updated A-OK";
+			}
+		}
+	} elseif((empty($_SESSION["profile"]) === false) && (($_SESSION["profile"]->getProfileId()) === $id) && (($_SESSION["profile"]->getProfileType()) === "a") || (($_SESSION["profile"]->getProfileType())) === "o") {
+
+		if($method === "DELETE") {
+			verifyXsrf();
+
+			//retrieve the company to be deleted
+			$company = Company::getCompanyByCompanyId($pdo, $id);
+
+			//check if empty
+			if($company === null) {
+				throw(new RuntimeException("The company does not exist", 404));
+			}
+
+			//delete the company
+			$company->delete($pdo);
+
+			//update the reply
+			$reply->message = "Company deleted A-OK";
+		} else {
+			throw (new InvalidArgumentException("Invalid HTTP method request"));
 		}
 
-	} elseif($method === "DELETE") {
-		verifyXsrf();
-
-		//retrieve the company to be deleted
-		$company = Company::getCompanyByCompanyId($pdo, $id);
-
-		//check if empty
-		if($company === null) {
-			throw(new RuntimeException("The company does not exist", 404));
-		}
-
-		//delete the company
-		$company->delete($pdo);
-
-		//update the reply
-		$reply->message = "Company deleted A-OK";
-	} else {
-		throw (new InvalidArgumentException("Invalid HTTP method request"));
 	}
-
-
 	//update the reply with exception information
 } catch(Exception $exception) {
 	$reply->status = $exception->getCode();
