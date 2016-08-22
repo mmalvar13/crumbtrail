e4a895debe90c5f63b5dbf79ad77f1298c16485e
Victoria Chacon <Vchaco06@gmail.com>
2016-08-22 14:42:24 -0600

    Wrote in the dockblcok for the Employee api. Also verified the session.
    https://app.asana.com/0/161552024814178/169698496837389

---
+++ public_html/php/apis/employee/index.php
@@ -0,0 +1,28 @@
+<?php
+
+//Api for the employee class
+
+//POST a new employee?
+
+
+//GET employee?
+//do i need my cnm user id for "use"
+
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\CrumbTrail\ {Employ, Profile};
+
+/**
+ * api for the Employee class
+ *
+ * @author Victoria Chacon <victoriousdesignco@gmail.com>
+ **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+
+}
