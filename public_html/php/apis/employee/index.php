b7a9d9ad47d9977e470438b9569246295a69e117
Victoria Chacon <Vchaco06@gmail.com>
2016-08-24 15:47:54 -0600

    Added in POST and DELETE https://app.asana.com/0/161552024814178/169698496837389

--- public_html/php/apis/employee/index.php
+++ public_html/php/apis/employee/index.php
@@ -11,7 +11,9 @@ require_once "autoloader.php";
 require_once "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\CrumbTrail\ {Employ, Profile};
+use Edu\Cnm\CrumbTrail\ {
+	Employ, Profile
+};
 
 /**
  * api for the Employee class
@@ -43,9 +45,108 @@ try {
 	$employProfileId = filter_input(INPUT_GET, "employProfileId", FILTER_SANITIZE_STRING);
 	$employCompanyId = filter_input(INPUT_GET, "employCompanyId", FILTER_SANITIZE_STRING);
 	//make sure the id is valid for methods that require it
-	If(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id <0)) {
+	If(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
 
 	//handle GET request - if id is present, that employee is returned, otherwise all employees get returned???
+	if($method === "GET") {
+		//set XSRF cookie
+		setXsrfCookie();
+
+		//get specific employee or all employees and update reply
+		if(empty($id) === false) {
+			$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $id);
+			if($employ !== null) {
+				$reply->data = $employ;
+			}
+		} elseif((empty($id)) === false) {
+			$employ = Employ::getEmployByEmployProfileId($pdo, $id);
+			if($employ !== null) {
+				{
+					$reply->data = $employ;
+				}
+			} elseif
+			((empty($id)) === false
+			) {
+				$employ = Employ::getEmployByEmployCompanyId($pdo, $id);
+				if($employ !== null) {
+					{
+						$reply->data = $employ;
+					}
+				} else {
+					$employs = Employ::getAllEmploys($pdo);
+					if($employs !== null) {
+						$reply->data = $employs;
+					}
+				}
+			} elseif(($method === "PUT" || "POST")) {
+				//verify Xsrf
+				$requestContent = file_get_contents("php://input");
+				$requestObject = json_decode($requestContent);
+
+				//make sure employ profile is available
+				if(empty($requestObject->employProfileId) === true) {
+					throw(new InvalidArgumentException("No profile exists.", 405));
+				}
+				//make sure employ company id exists
+				if(empty($requestObject->employCompanyId) === true) {
+					throw(new InvalidArgumentException("No company exists.", 405));
+				}
+				//make sure the company id and the profile id exist
+				if(empty($requestObject->employ) === true) {
+					throw(new InvalidArgumentException("No employ Company id and employ Profile combination exists ", 405));
+				}
+
+				//perform actual PUT or POST
+
+				if($method === "POST") {
+					$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $id);
+					if($employ === null) {
+						////create new relationship between profile and company id
+						$employ = new Employ($requestObject->employCompanyId, $requestObject->employProfileId);
+						$employ->insert($pdo);
+						$profile = Profile::getProfileByProfileId($pdo, $requestObject->employProfileId);
+						$profile->setProfileType($requestObject->profileType);
+						$profile->update($pdo);
+						// update profile here
+					} else {
+						throw (new InvalidArgumentException("This profile Id and Profile type combination does not exist.", 405));
+					}
+				}
+			}
+		}
+	} elseif($method === "DELETE") {
+		verifyXsrf();
+		//retrieve employ to be deleted
+		//would I want employ by company and profile id, in the case that a worker/co owner is part of many companies??
+		$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $id);
+		if($employ === null) {
+			throw(new RuntimeException("No employ Company Id and employ Profile Id combination exists.", 404));
+		}
+		//delete employ
+		$employ->delete($pdo);
+		//update reply
+		$reply->message = "Employee or CoOwner deleted successfully";
+
+	} else {
+		throw (new InvalidArgumentException("Invalid HTTP method Request"));
+	}
+	//update reply with exception information
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->trace = $exception->getTraceAsString();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
 }
+
+header("Content-type: application.json");
+if($reply->data === null) {
+	unset($reply->data);
+}
+//encode and return reply to front end caller
+echo json_encode($reply);
+
+
