089b029984d9638ce0444db2ca7a363699580d3a
Monica Alvarez <mmalvar13@gmail.com>
2016-08-23 19:10:04 -0600

    made sure required fields were entered, created new profile/company, begin swiftmailer https://app.asana.com/0/161555263951748/169698496837351

--- public_html/php/apis/signUp/index.php
+++ public_html/php/apis/signUp/index.php
@@ -42,39 +42,102 @@ try {
 		$requestContent = file_get_contents("php://input"); //what is the directory reference here??
 		$requestObject = json_decode($requestContent);
 
-		//make sure profileId is empty because we are creating a new profile
-		//is this where we throw exceptions for email and stuff?
-		if(empty($requestObject->profileId) === false) {
-			throw(new \InvalidArgumentException("This profile already exists", 405));
+		//make sure all required fields are entered
+		if(empty($requestObject->profileName) === true){
+			throw(new \InvalidArgumentException("Must provide your name", 405));
 		}
-		//make sure comapnyId is empty because we are signing up a new company
-		if(empty($requestObject->companyId) === false) {
-			throw(new \InvalidArgumentException("This company already exists", 405));
+		if(empty($requestObject->profileEmail) === true ){
+			throw(new \InvalidArgumentException("Must provide an email address", 405)); //what error code would i throw. what do i compare the inserted profile email too? do I have to traverse an array of getAllEmails???
 		}
-		//make sure profileEmail is unique
-		if(empty($requestObject->profileEmail) ===false ){
-			throw(new \InvalidArgumentException("This email is already attached to another profile", 405)); //what error code would i throw. what do i compare the inserted profile email too? do I have to traverse an array of getAllEmails???
+		if(empty($requestObject->profilePhone)=== true){
+			throw(new \InvalidArgumentException("Must provide a phone number", 405));
 		}
-		//make sure comapny business license is unique
-		if(empty($requestObject->companyLicense) === false){
-			throw(new \InvalidArgumentException("A company already exists under this business license", 405));
+		if(empty($requestObject->profileType)=== true){
+			$requestObject->profileType = "o"; //is this correct? anyone going this route is going to be an owner automatically right????
+		}
+		if(empty($requestObject->companyName)=== true){
+			throw(new \InvalidArgumentException("You must enter a company name", 405));
+		}
+		if(empty($requestObject->companyEmail)===true){
+			throw(new\InvalidArgumentException("You must enter a company email address", 405));
+		}
+		if(empty($requestObject->companyPhone)=== true){
+			throw(new \InvalidArgumentException("You must enter a company phone number", 405));
+		}
+		if(empty($requestObject->companyPermit)=== true){
+			throw(new \InvalidArgumentException("You must enter your businesses health permit number", 405));
+		}
+		if(empty($requestObject->companyLicense) === true){
+			throw(new \InvalidArgumentException("Must provide a company business license number", 405));
+		}
+		if(empty($requestObject->companyAttn)=== true){
+			throw(new \InvalidArgumentException("Must provide company attention contact", 405));
+		}
+		if(empty($requestObject->companyStreet1) === true){
+			throw(new \InvalidArgumentException("Must provide company address", 405));
+		}
+		if(empty($requestObject->companyStreet2)=== true){
+			$requestObject->companyStreet2 = null;
+		}
+		if(empty($requestObject->companyCity)=== true){
+			throw(new \InvalidArgumentException("Must provide city", 405));
+		}
+		if(empty($requestObject->companyState)=== true){
+			throw(new \InvalidArgumentException("Must provide state", 405));
+		}
+		if(empty($requestObject->companyZip)=== true){
+			throw(new \InvalidArgumentException("Must provide zip code", 405));
+		}
+		if(empty($requestObject->companyDescription) === true){
+			$requestObject->companyDescription = null;
+		}
+		if(empty($requestObject->companyMenuText) === true){
+			$requestObject->companyMenuText = null;
 		}
 
-		//what other exceptions do i throw here? what am i trying to catch here? and what do i try to catch in angular??
+		//sanitize the email
+		$profileEmail = filter_var($requestObject->profileEmail, FILTER_SANITIZE_EMAIL);
+		$profile = Profile::getProfileByProfileEmail($pdo, $profileEmail);
+		if($profile !== null){
+			throw(new \InvalidArgumentException("this email already has an account",422);
+		}
+
 		//before hashing and salting, angular sends password and confirmed password. throw exception if they are not the same. requestObject->password/confirm password
 		//hash and salt it here. same as in the test. whatever is in the setup method in test
 
 		//create a new profile and insert it into the databases
-		$profile = new Profile(null, $requestObject->profileName, $requestObject->profileEmail, $requestObject->profilePhone, $requestObject->profileAccessToken, $requestObject->profileActivationToken, $requestObject->profileType, $requestObject->profileHash, $requestObject->profileSalt);
+		$profile = new Profile(null, $requestObject->profileName, $requestObject->profileEmail, $requestObject->profilePhone, $profileAccessToken, $profileActivationToken, $requestObject->profileType, $profileHash, $profileSalt);
 
 		$profile->insert($pdo);
 
-		//here i have companyDescription and companyMenuText.
-		$company = new Company(null, $requestObject->companyAccountCreatorId, $requestObject->companyName, $requestObject->companyEmail, $requestObject->companyPhone, $requestObject->companyPermit, $requestObject->companyLicense, $requestObject->companyAttn, $requestObject->companyStreet1, $requestObject->companyStreet2, $requestObject->companyCity, $requestObject->companyState, $requestObject->companyZip, null , null, $requestObject->companyActivationToken, $requestObject->companyApproved);
+		//create a new company and insert it into the database
+		$company = new Company(null, $profile->getProfileId(), $requestObject->companyName, $requestObject->companyEmail, $requestObject->companyPhone, $requestObject->companyPermit, $requestObject->companyLicense, $requestObject->companyAttn, $requestObject->companyStreet1, $requestObject->companyStreet2, $requestObject->companyCity, $requestObject->companyState, $requestObject->companyZip, $requestObject->companyDescription , $requestObject->companyMenuText, $companyActivationToken, $companyApproved);
 
 		$company->insert($pdo);
 		//swiftmailer code here
 
+		//create Swift message
+		$swiftMessage = Swift_Message::newInstance();
+
+		//attach the sender to the message
+		//this takes the form of an associative array where the Email is the key for the real name
+		$swiftMessage->setFrom(["crumbtrail@gmail.com" => "CrumbTrail"]);
+
+		/**
+		 * attach the recipients to the message
+		 * notice this is an array that can inlcude or omit the recipient's real name
+		 * use the recipient's real name where possible; this reduces the probability of the Email being marked as spam
+		 **/
+		$recipients = [$requestObject->profileEmail];
+		$swiftMessage->setTo($recipients);
+
+		//attach the subject line to the message
+		$swiftMessage->setSubject("Please confirm your CrumbTrail account to activate");
+
+		/**
+		 * attach the actual message to the message
+		 * here, we set two versions of the message: the HTML formatted mess
+		 **/
 		//update reply
 		$reply->message = "Check your email to activate";
 
