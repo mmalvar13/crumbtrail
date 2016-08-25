f098e5f03a8d2412d6e8c2180156f6409f9024d8
Kevin Lee Kirk <klkirkhome@gmail.com>
2016-08-25 13:44:41 -0600

    Sketched in the outline of what we need Swift Mailer to do, in company activation API. https://app.asana.com/0/161552024814156/169698496837365

--- public_html/php/apis/companyActivation/index.php
+++ public_html/php/apis/companyActivation/index.php
@@ -2,9 +2,6 @@
 /**
  * API for Company Activation.
  * @author Kevin Lee Kirk
-
- * The response to a activation email to a new company owner (the companyAccountCreator).
- * So this API needs to receive information from the activation email.
  *
  * The sign-up API sends the new company owner an activation email.
  * The new company owner clicks a link in the activation email.
@@ -14,43 +11,74 @@
  * How much of this does Angular do?
  * In what form does the information from Angular arrive?
  *
+ * Must do this:
+ * getCompanyByCompanyActivationToken
+ * check that there is an activation token
+ * then set the activation token = null
  **/
+
 require_once "autoloader.php";
 require_once "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-
-// TODO A require_once for  swiftmailer/mailgun?
+require_once '/path/to/swift-mailer/lib/swift_required.php';  // TODO Path?
 
 use Edu\Cnm\Crumbtrail\{Company};
+
+// TODO SwiftMailer things: ----------------------------------------
+
+// Create the Transport
+$transport = Swift_SmtpTransport::newInstance('smtp.example.org', 25)
+	->setUsername('your username')
+	->setPassword('your password')
+;
+
+// Create the Mailer using your created Transport
+$mailer = Swift_Mailer::newInstance($transport);
+
+// Create a message
+$message = Swift_Message::newInstance('Wonderful Subject')
+	->setFrom(array('john@doe.com' => 'John Doe'))
+	->setTo(array('receiver@domain.org', 'other@domain.org' => 'A name'))
+	->setBody('Here is the message itself')
+;
+
+// Send the message
+$result = $mailer->send($message);
+
+// TODO: How to include an activation link that the new company account creator clicks???
+// And what responds to that click?  This API?  Another API?
+
+
+
+
 
 // Verify the session, start a session if not active.
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
-
 // Prepare an empty $reply "bucket".
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
-
-
-// TODO Insert code for receiving the information that the activation email link has been clicked.
-
 
 try{
 	// Get the mySQL connection.
 	$pdo = connectToEncyptedMySQL("/etc/apache2/capstone-mysql/companyActivation.ini"); // TODO Check path.
 
 	// Determine which HTTP method was used. first one is the method you use, second is the fall back if the first is not available.
-	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"]; //do i use this?
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	// Only accept GET method.  Reject all other methods. ???
 
 	// Sanitize the input.
 	$companyAccountCreatorEmailActivation = filter_input(INPUT_GET, "companyAccountCreatorEmailActivation", FILTER_SANITIZE_STRING);
 
 	$company = Company::getCompanyByCompanyAccountCreatorId($pdo, $companyAccountCreatorEmailActivation);
+	// Or:
+	$company = Company::getCompanyByCompanyActivationToken($pdo, $companyActivationToken);
 
 
-	//handle GET request - if id is present, that profile is returned, otherwise all profiles are returned??
+	//handle GET request - if id is present, that profile is returned
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
@@ -60,7 +88,7 @@ try{
 			throw(new InvalidArgumentException("Account has already been activated or does not exist", 404));
 
 		} else {
-			$companyAccountCreatorEmailActivation->setCompanyAccountCreatorId(null);
+			$companyAccountCreatorEmailActivation->setCompanyActivationToken(null);  // ???
 			$companyAccountCreatorEmailActivation->update($pdo);
 
 			// TODO After receiving the click from the activation email,
@@ -69,3 +97,7 @@ try{
 		}
 	}
 }
+
+
+// Encode and return reply to front end caller.
+echo json_encode($reply);
