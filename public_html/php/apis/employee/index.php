6fc414d5237043e6688544668ed6f5793780f114
Victoria Chacon <Vchaco06@gmail.com>
2016-08-28 17:18:54 -0600

    fixed the bracket problem
    https://app.asana.com/0/161552024814178/169698496837389

--- public_html/php/apis/employee/index.php
+++ public_html/php/apis/employee/index.php
@@ -105,36 +105,36 @@ try {
 				//make sure employ company id exists
 				//--if(empty($requestObject->employCompanyId) === true) {
 				//--	throw(new InvalidArgumentException("No company exists.", 405));
-				}
+				//}
 				//make sure the company id and the profile id exist
 				//--if(empty($requestObject->employ) === true) {
 					//--throw(new InvalidArgumentException("No employ Company id and employ Profile combination exists ", 405));
-				}
+				//}
 				//here??
 				//--if(empty($requestObject->profileName) === true) {
 					//--throw(new InvalidArgumentException("No profile name exists", 405));
-				}
+				//}
 
 
 				//perform actual PUT or POST
 /*-------------------------------WHy is this here again?---------------*/
-				if($method === "POST") {
+				//if($method === "POST") {
 					//makes sure tht everything I put in filter_sanitize exists
-					$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $employCompanyId, $employProfileId);
-					if($employ === null) {
+					//$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $employCompanyId, $employProfileId);
+					//if($employ === null) {
 						////create new relationship between profile and company id
-						$employ = new Employ($requestObject->employCompanyId, $requestObject->employProfileId);
-						$employ->insert($pdo);
-						$profile = Profile::getProfileByProfileId($pdo, $requestObject->employProfileId);
-						$profile->setProfileType($requestObject->profileType);
-						$profile->update($pdo);
+						//$employ = new Employ($requestObject->employCompanyId, $requestObject->employProfileId);
+						//$employ->insert($pdo);
+						//$profile = Profile::getProfileByProfileId($pdo, $requestObject->employProfileId);
+						//$profile->setProfileType($requestObject->profileType);
+						//$profile->update($pdo);
 						// update profile here
-					} else {
-						throw (new InvalidArgumentException("This profile Id and Profile type combination does not exist.", 405));
+					//} else {
+						//throw (new InvalidArgumentException("This profile Id and Profile type combination does not exist.", 405));
 					}
 				}
-			}
-		}
+			//}
+		//}
 	} elseif($method === "DELETE") {
 		verifyXsrf();
 		//retrieve employ to be deleted
