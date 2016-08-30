8d912d53a2d89af6387892dd822f44586365c326
Monica Alvarez <mmalvar13@gmail.com>
2016-08-30 16:45:27 -0600

    some great changes https://app.asana.com/0/161552024814189/169698496837375

--- public_html/php/apis/companyActivation/index.php
+++ public_html/php/apis/companyActivation/index.php
@@ -15,7 +15,9 @@ require_once "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once (dirname(__DIR__, 4)) . "/vendor/autoload.php";
 
-use Edu\Cnm\Crumbtrail\{Company};
+use Edu\Cnm\Crumbtrail\{
+	Company
+};
 
 // Verify the session, start a session if not active.
 if(session_status() !== PHP_SESSION_ACTIVE) {
@@ -29,80 +31,75 @@ $reply->data = null;
 
 try {
 	// Get the mySQL connection.
-	$pdo = connectToEncyptedMySQL("/etc/apache2/capstone-mysql/companyActivation.ini"); // TODO Check path.
+	$pdo = connectToEncyptedMySQL("/etc/apache2/capstone-mysql/crumbtrail.ini");
 
 	// Determine which HTTP method was used.
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
-	// Only accept a GET request.  Catch all other methods and throw exceptions.
+	$companyActivationToken = filter_input(INPUT_GET, "companyActivationToken", FILTER_SANITIZE_STRING);
+
+	// Only accept a GET request.  Catch all other methods and exceptions.
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
 
-		$company = Company::getCompanyByCompanyId($pdo, $companyId);
-		$companyEmail = Company($pdo, $companyEmail);
-		$companyApproved = Company($pdo, $companyApproved);
-		$companyActivationToken = Company($pdo, $companyActivationToken);
+		$company = Company::getCompanyByCompanyActivationToken($pdo, $companyActivationToken);
+
 	}
 
-		try {
-			if($companyActivationToken !== null) {
-				$companyActivationToken = null;
-			}
-		} catch (Exception $exception) {
-			$reply->status = $exception->getCode();
-			$reply->message = $exception->getMessage();
-		} catch (TypeError $typeError) {
-			$reply->status = $typeError->getCode();
-			$reply->message = $typeError->getMessage();
-		}
+} catch(Exception $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
 
-	} catch (Exception $exception) {
-		$reply->status = $exception->getCode();
-		$reply->message = $exception->getMessage();
-
-	} catch (TypeError $typeError) {
-		$reply->status = $typeError->getCode();
-		$reply->message = $typeError->getMessage();
+} catch(TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
 }
+
+// check comActTok not null, then set to null
+
+if($requestObject->companyApproved === null) {
+	throw(new \RuntimeException('company has not been approved yet'));
+} else {
 
 // ------------ SwiftMailer: send Approve or Deny email to companyAccountCreator ------------
 //Create the Transport
-$transport = Swift_SmtpTransport::newInstance('smtp.example.org', 25); //can add third parameter for SSL encryption. need?
+	$transport = Swift_SmtpTransport::newInstance('localhost', 25); //can add third parameter for SSL encryption. need?
 
 //Create the Mailer using your created Transport
-$mailer = Swift_Mailer::newInstance($transport);
+	$mailer = Swift_Mailer::newInstance($transport);
 
 //Create a message
-$message = Swift_Message::newInstance();
+	$message = Swift_Message::newInstance();
 
 //attach a sender to the message
-$message->setFrom(['admin@crumbtrail.com' => 'Crumbtrail Admin']);
+	$message->setFrom(['admin@crumbtrail.com' => 'Crumbtrail Admin']);
 
 //attach recipients to the message. you can add
-$recipients = ['companyEmail' => '???'];			// TODO ???
-$message->setTo($recipients);	//we will just send to one person.
+	$recipients = ['companyEmail' => $company->getCompanyEmail()];
+//$message->setTo($recipients);	//we will just send to one person.
 
 //attach a subject line to the message
-$message->setSubject("Message from CrumbTrail");
+	$message->setSubject("Message from CrumbTrail");
 
 //the body of the message-seen when the user opens the message
-if($companyApproved = 1) {
-	$message->setBody('Welcome to CrumbTrail! Your company account has been approved. Please go to crumbtrail.com to add the description and menu of your food truck company.', 'text/html');
-} else {
-	$message->setBody('CrumbTrail has been unable to verify your business license and/or health permit.', 'text/html');
-}
+	if($company->getCompanyApproved() === 1) {
+		$message->setBody('Welcome to CrumbTrail! Your company account has been approved. Please go to crumbtrail.com to add the description and menu of your food truck company.', 'text/html');
+	} else {
+		$message->setBody('CrumbTrail has been unable to verify your business license and/or health permit.', 'text/html');
+	}
+
 
 //Send the message
-$numSent = $mailer->send($message);
+	$numSent = $mailer->send($message);
 
-printf("Sent %d messages\n", $numSent);
+	printf("Sent %d messages\n", $numSent);
 
-if($numSent !== count($recipients)) {
-	//the $failedRecipients parameter passed in the send() method now contains an array of the Emails that failed
-	throw(new RuntimeException("unable to send email"));
+	if($numSent !== count($recipients)) {
+		//the $failedRecipients parameter passed in the send() method now contains an array of the Emails that failed
+		throw(new RuntimeException("unable to send email"));
+	}
+	/*----------------------------------SwiftMailer Code Ends Here------------------------------------------*/
 }
-/*----------------------------------SwiftMailer Code Ends Here------------------------------------------*/
-
 // Encode and return reply to front end caller.
 echo json_encode($reply);
