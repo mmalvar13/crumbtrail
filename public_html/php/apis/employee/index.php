748eda96fd49d3ae508a8367b7e40df3ffe3f2a0
Victoria Chacon <Vchaco06@gmail.com>
2016-08-31 10:06:46 -0600

    changed the setBody... https://app.asana.com/0/161552024814178/169698496837389

--- public_html/php/apis/employee/index.php
+++ public_html/php/apis/employee/index.php
@@ -135,7 +135,7 @@ try {
 				$message->setSubject('You have been invited to join crumbtrail. Please verify your email.');
 
 				//the body of the message-seen when the user opens the message
-				$message->setBody('Thanks for signing your company up with Crumbtrail. Please confirm your email by clicking on this link. Once you have confirmed your email, Crumbtrail admins can get to work on verifying your business and approving you to use the app. You will recieve an email when you have been approved.  ','text/html');
+				$message->setBody('Thank you for joining crumbtrail. Please confirm your email by clicking on this link.','text/html');
 
 				//add alternative parts with addPart() meant for those who cannot read in html???
 				$message->addPart('Thanks for signing your company up with Crumbtrail. Please confirm your email by clicking on this link. Once you have confirmed your email, Crumbtrail admins can get to work on verifying your business and approving you to use the app. You will recieve an email when you have been approved.  ', 'text/plain');
