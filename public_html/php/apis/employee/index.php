cc1e4037085fa8339a2dc393971a3b8cddc02e65
Kevin Lee Kirk <klkirkhome@gmail.com>
2016-08-31 15:51:20 -0600

    Changes to brackets oh no!

--- public_html/php/apis/employee/index.php
+++ public_html/php/apis/employee/index.php
@@ -7,13 +7,15 @@
 //do i need my cnm user id for "use"
 
 
-require_once(dirname(__DIR__,2) . "/classes/autoloader.php");
-require_once(dirname(__DIR__,2) . "/lib/xsrf.php");
+require_once(dirname(__DIR__, 2) . "/classes/autoloader.php");
+require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-require_once (dirname(__DIR__,4) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
 
 
-use Edu\Cnm\CrumbTrail\ {Employ, Profile};
+use Edu\Cnm\CrumbTrail\ {
+	Employ, Profile, Company
+};
 
 /**
  * api for the Employee class
@@ -32,9 +34,8 @@ $reply->status = 200;
 $reply->data = null;
 
 try {
-	//grab mySQL conection
-	$pdo = connectToEncryptedMySQL
-	("/etc/apache2/capstone-mysql/crumbtrail.ini");
+	//grab mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/crumbtrail.ini");
 
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP-METHOD"] : $_SERVER["REQUEST_METHOD"];
@@ -42,15 +43,13 @@ try {
 	//sanitize input
 	//using the Get method here...so I am not writing the "employ class" but a bridge between employ and profile?
 //we wouldn't need an Id since this wouldn't be tracked right, this is destroyed once the employee verifies that they are part of the profile?
+	//does angular know about this field? input get access url parameters...can we search on this field??
 	$employProfileId = filter_input(INPUT_GET, "employProfileId", FILTER_VALIDATE_INT);
 	$employCompanyId = filter_input(INPUT_GET, "employCompanyId", FILTER_VALIDATE_INT);
-	$profileName = filter_input(INPUT_POST, "profileName", FILTER_SANITIZE_STRING);
-	$profileType = filter_input(INPUT_POST, "profileType", FILTER_SANITIZE_STRING);
-	$profileEmail = filter_input(INPUT_POST, "profileEmail", FILTER_SANITIZE_EMAIL);
-	//may possible write in GET to link to company------//
+	//may possible write in GET to link to company------OUR LINK TO COMPANY IS THE EMPLOYCOMPANYID//
 	//make sure the id is valid for methods that require it
-	//TODO: if an owner takes an employee of their profile how does that work? DO i need the delete method, maybe set profile type to null, and possibly i  a PUT method if we wanted to dirupt the connedction betweent employee and company profile...."//
-	If(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+	//TODO: if an owner takes an employee of their profile how does that work? DO i need the delete method, maybe set profile type to null, and possibly a PUT method if we wanted to disrupt the connection between employee and company profile....DELETE $employ, in teh delete method?"//
+	if(($method === "DELETE") && (empty($id) === true || $id < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
 
@@ -60,150 +59,142 @@ try {
 		setXsrfCookie();
 //TODO: we get the company by.....so we cant attach to the company...
 		//get specific employee or all employees and update reply
-		if(empty($id) === false) {
+		if(empty($employCompanyId) === false && empty($employProfileId) === false) {
 			$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $employCompanyId, $employProfileId);
 			if($employ !== null) {
 				$reply->data = $employ;
 			}
-		} elseif((empty($id)) === false) {
+		} elseif((empty($employProfileId)) === false) {
 			$employ = Employ::getEmployByEmployProfileId($pdo, $employProfileId);
 			if($employ !== null) {
-				{
-					$reply->data = $employ;
-				}
-			} elseif
-			((empty($id)) === false) {
-				$employ = Employ::getEmployByEmployCompanyId($pdo, $employCompanyid);
-				if($employ !== null) {
-					{
-						$reply->data = $employ;
-					}
-				} else {
-					$employs = Employ::getAllEmploys($pdo);
-					if($employs !== null) {
-						$reply->data = $employs;
-					}
-				}
-			} elseif(($method ===  "POST")) {
-				//verify Xsrf
-				$requestContent = file_get_contents("php://input");
-				$requestObject = json_decode($requestContent);
+				$reply->data = $employ;
+			}
+		} elseif((empty($employCompanyId)) === false) {
+			$employ = Employ::getEmployByEmployCompanyId($pdo, $employCompanyId);
+			if($employ !== null) {
+				$reply->data = $employ;
+			}
+		}
+	} elseif(($method === "POST")) {
+		//verify Xsrf
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
 
-				//make sure employ profile is available
-				if(empty($requestObject->profileName) === true) {
-					throw(new InvalidArgumentException("Need to input employee name.", 405));
-				}
-				if(empty($requestObject->profileType) === true) {
-					throw(new InvalidArgumentException("Need to input the owner or employee status.", 405));
-				}
-				if(empty($requestObject->profileEmail) === true) {
-					throw(new InvalidArgumentException("Need to insert employee email.", 405));
-				}
-				//Todo----GET COMPANY BY COMPANYEMAIL....POSSIBLY....IF WE NEED TO LINK BACK TO COMPANY
-				//-----here im making Salt, DummyPassword, profile activation token and hash---//
-				$salt = bin2hex(random_bytes(16));
-				$dummyPassword = bin2hex(random_bytes(16));
-				$profileActivationToken = bin2hex(random_bytes(16));
-				//what is the number at the end of the hash??
-				$hash = hash_pbkdf2("sha512", $dummyPassword, $salt, 262144);
+		//make sure employ profile is available
+		if(empty($requestObject->profileName) === true) {
+			throw(new InvalidArgumentException("Need to input employee name.", 405));
+		}
+		if(empty($requestObject->profileType) === true) {
+			throw(new InvalidArgumentException("Need to input the owner or employee status.", 405));
+		}
+		if(empty($requestObject->profileEmail) === true) {
+			throw(new InvalidArgumentException("Need to insert employee email.", 405));
+		}
+		//Todo----GET COMPANY BY COMPANYEMAIL....POSSIBLY....IF WE NEED TO LINK BACK TO COMPANY
+		//-----here im making Salt, DummyPassword, profile activation token and hash---//
+		$salt = bin2hex(random_bytes(16));
+		$dummyPassword = bin2hex(random_bytes(16));
+		$profileActivationToken = bin2hex(random_bytes(16));
+		//what is the number at the end of the hash??
+		$hash = hash_pbkdf2("sha512", $dummyPassword, $salt, 262144);
 
-				//created new profile and insert into database
-				$profile = new Profile(null, $requestObject->profileName, $requestObject->profileEmail,$profilePhone = "555-555-5555", $profileAccessToken = null, $profileActivationToken, $requestObject->profileType, $hash, $salt);
+		//created new profile and insert into database
+		$profile = new Profile(null, $requestObject->profileName, $requestObject->profileEmail, $profilePhone = "555-555-5555", $profileAccessToken = null, $profileActivationToken, $requestObject->profileType, $hash, $salt);
 
-				$profile->insert($pdo);
-				//update reply
-				$reply->message = "A link has been sent to your employee/CoOwner for verification.";
+		$profile->insert($pdo);
+		//update reply
+		$reply->message = "A link has been sent to your employee/CoOwner for verification.";
 
 //-----------------------------adding in swiftmailer------------------------------------//
-				//sends email between owner and employee??
-				$transport = Swift_SmtpTransport::newInstance('localhost', 25);
+		//sends email between owner and employee??
+		$transport = Swift_SmtpTransport::newInstance('localhost', 25);
 
-				//Create the Mailer using your created Transport
-				$mailer = Swift_Mailer::newInstance($transport);
+		//Create the Mailer using your created Transport
+		$mailer = Swift_Mailer::newInstance($transport);
 
-				//Create a message
-				$message = Swift_Message::newInstance();//to set a subject line you can pass it as a parameter in newInstance or set it afterwards. I chose to set it afterwards. Same with body.
+		//Create a message
+		$message = Swift_Message::newInstance();//to set a subject line you can pass it as a parameter in newInstance or set it afterwards. I chose to set it afterwards. Same with body.
 
-				//attach a sender to the message
-				$message->setFrom(['vchacon8@cnm.edu'=> 'Crumbtrail Admin']);//is this the same as setFrom(array('someaddress'=>'name'));
+		//attach a sender to the message
+		$message->setFrom(['vchacon8@cnm.edu' => 'Crumbtrail Admin']);//is this the same as setFrom(array('someaddress'=>'name'));
 
-				//attach recipients to the message. you can add
-				$recipients = [$profile->getProfileEmail() => $profile->getProfileName()];
-				$message->setTo($recipients);//we will just send to one person.
-				//include owner and employee name here???
-				//attach a subject line to the message
-				$message->setSubject('You have been invited to join crumbtrail. Please verify your email.');
+		//attach recipients to the message. you can add
+		$recipients = [$profile->getProfileEmail() => $profile->getProfileName()];
+		$message->setTo($recipients);//we will just send to one person.
+		//include owner and employee name here???
+		//attach a subject line to the message
+		$message->setSubject('You have been invited to join crumbtrail. Please verify your email.');
 
-				//the body of the message-seen when the user opens the message
-				$message->setBody('Thank you for joining crumbtrail. Please confirm your email by clicking on this link.','text/html');
+		//the body of the message-seen when the user opens the message
+		$message->setBody('Thank you for joining crumbtrail. Please confirm your email by clicking on this link.', 'text/html');
 
-				//add alternative parts with addPart() meant for those who cannot read in html???
-				$message->addPart('Thank you for joining crumbtrail. Please confirm your email by clicking on this link. ', 'text/plain');
-				$message->setReturnPath('bounces@address.tld');//return path address specifies where bounce notifications should be sent
-
-
-				//Link that will be clicked on to confirm the employess email address? and set their profileActivationToken to null. This triggers an email (over in profileActivation API)
-
-				//you should use $_SERVER["SCRIPT_NAME"]
-				$scriptPath = $_SERVER["SCRIPT_NAME"];
-				$linkPath = dirname($scriptPath, 2) . "/profileActivation/?profileActivationToken =$profileActivationToken";
+		//add alternative parts with addPart() meant for those who cannot read in html???
+		$message->addPart('Thank you for joining crumbtrail. Please confirm your email by clicking on this link. ', 'text/plain');
+		$message->setReturnPath('bounces@address.tld');//return path address specifies where bounce notifications should be sent
 
 
-				//Send the message
-				$numSent = $mailer->send($message);
+		//Link that will be clicked on to confirm the employess email address? and set their profileActivationToken to null. This triggers an email (over in profileActivation API)
 
-				printf("Sent %d messages\n", $numSent);
+		//you should use $_SERVER["SCRIPT_NAME"]
+		$scriptPath = $_SERVER["SCRIPT_NAME"];
+		$linkPath = dirname($scriptPath, 2) . "/profileActivation/?profileActivationToken =$profileActivationToken";
 
-				/**
-				 * the send method returns the number of recipients that accepted the Email
-				 * so if the number attempted is not the number accepted, this is the exception (number attempted should only be one at a time)
-				 **/
-				if($numSent !== count($recipients)){
-					//the $failedRecipients parameter passed in the send() method now contains an array of the Emails that failed
-					throw(new RuntimeException("unable to send email"));
-				}
+
+		//Send the message
+		$numSent = $mailer->send($message);
+
+		printf("Sent %d messages\n", $numSent);
+
+		/**
+		 * the send method returns the number of recipients that accepted the Email
+		 * so if the number attempted is not the number accepted, this is the exception (number attempted should only be one at a time)
+		 **/
+		if($numSent !== count($recipients)) {
+			//the $failedRecipients parameter passed in the send() method now contains an array of the Emails that failed
+			throw(new RuntimeException("unable to send email"));
+		}
 //-----------------------------------------------SWIFTMAILER END-----------------------------------------------//
-				//------DO I NEED ALL OF THIS HERE???WHY???-------//
-				//---if(empty($requestObject->employProfileId) === true) {
-				//--	throw(new InvalidArgumentException("No profile exists.", 405));
-				//--}
-				//make sure employ company id exists
-				//--if(empty($requestObject->employCompanyId) === true) {
-				//--	throw(new InvalidArgumentException("No company exists.", 405));
-				//}
-				//make sure the company id and the profile id exist
-				//--if(empty($requestObject->employ) === true) {
-					//--throw(new InvalidArgumentException("No employ Company id and employ Profile combination exists ", 405));
-				//}
-				//here??
-				//--if(empty($requestObject->profileName) === true) {
-					//--throw(new InvalidArgumentException("No profile name exists", 405));
-				//}
+		//------DO I NEED ALL OF THIS HERE???WHY???-------//
+		//---if(empty($requestObject->employProfileId) === true) {
+		//--	throw(new InvalidArgumentException("No profile exists.", 405));
+		//--}
+		//make sure employ company id exists
+		//--if(empty($requestObject->employCompanyId) === true) {
+		//--	throw(new InvalidArgumentException("No company exists.", 405));
+		//}
+		//make sure the company id and the profile id exist
+		//--if(empty($requestObject->employ) === true) {
+		//--throw(new InvalidArgumentException("No employ Company id and employ Profile combination exists ", 405));
+		//}
+		//here??
+		//--if(empty($requestObject->profileName) === true) {
+		//--throw(new InvalidArgumentException("No profile name exists", 405));
+		//}
 
 
-				//perform actual PUT or POST
-/*-------------------------------WHy is this here again?---------------*/
-/*-----------NEED TO GET THIS EXTRA POST OUT---------------------------*/
-				//if($method === "POST") {
-					//makes sure tht everything I put in filter_sanitize exists
-					//$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $employCompanyId, $employProfileId);
-					//if($employ === null) {
-						////create new relationship between profile and company id
-						//$employ = new Employ($requestObject->employCompanyId, $requestObject->employProfileId);
-						//$employ->insert($pdo);
-						//$profile = Profile::getProfileByProfileId($pdo, $requestObject->employProfileId);
-						//$profile->setProfileType($requestObject->profileType);
-						//$profile->update($pdo);
-						// update profile here
-					//} else {
-						//throw (new InvalidArgumentException("This profile Id and Profile type combination does not exist.", 405));
-					}
-				}
-			//}
+		//perform actual PUT or POST
+		/*-------------------------------WHy is this here again?---------------*/
+		/*-----------NEED TO GET THIS EXTRA POST OUT---------------------------*/
+		//if($method === "POST") {
+		//makes sure tht everything I put in filter_sanitize exists
+		//$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $employCompanyId, $employProfileId);
+		//if($employ === null) {
+		////create new relationship between profile and company id
+		//$employ = new Employ($requestObject->employCompanyId, $requestObject->employProfileId);
+		//$employ->insert($pdo);
+		//$profile = Profile::getProfileByProfileId($pdo, $requestObject->employProfileId);
+		//$profile->setProfileType($requestObject->profileType);
+		//$profile->update($pdo);
+		// update profile here
+		//} else {
+		//throw (new InvalidArgumentException("This profile Id and Profile type combination does not exist.", 405));
+	}
+}
+		//}
 		//}
 		//TODO: Do we need this here
-	} elseif($method === "DELETE") {
-		verifyXsrf();
+	} elseif($method === "DELETE"){
+verifyXsrf();
 		//retrieve employ to be deleted
 		//would I want employ by company and profile id, in the case that a worker/co owner is part of many companies??
 		$employ = Employ::getEmployByEmployCompanyIdAndEmployProfileId($pdo, $employCompanyId, $employProfileId);
@@ -216,8 +207,8 @@ try {
 		$reply->message = "Employee or CoOwner deleted successfully";
 
 	} else {
-		throw (new InvalidArgumentException("Invalid HTTP method Request"));
-	}
+	throw (new InvalidArgumentException("Invalid HTTP method Request"));
+}
 	//update reply with exception information
 } catch(Exception $exception) {
 	$reply->status = $exception->getCode();
