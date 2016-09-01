acb4a3557ed7202fea80325f39e81256bd35c5f8
Kevin Lee Kirk <klkirkhome@gmail.com>
2016-09-01 08:47:34 -0600

    Removed draft file from scratch directory

--- public_html/php/apis/profile/index.php
+++ public_html/php/apis/profile/index.php
@@ -11,7 +11,7 @@
 require_once(dirname(__DIR__, 2) . "/classes/autoload.php");
 require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
-
+require_once (dirname(__DIR__,4) . "/vendor/autoload.php");
 
 use Edu\Cnm\CrumbTrail\{Profile};
 
