de7822403669df072b44bdc44c236a5f6d0a32e4
Monica Alvarez <mmalvar13@gmail.com>
2016-09-01 17:32:50 -0600

    uncommented xsrf stuff https://app.asana.com/0/161555263951748/169698496837351

--- public_html/php/apis/signUp/index.php
+++ public_html/php/apis/signUp/index.php
@@ -56,8 +56,8 @@ try {
 
 	if($method === "POST") {
 		//set XSRF cookie
-//		setXsrfCookie(); //do we need this?
-//		verifyXsrf(); //todo what is the deal with this?
+		setXsrfCookie(); //do we need this?
+		verifyXsrf(); //todo what is the deal with this?
 
 		$requestContent = file_get_contents("php://input");
 		$requestObject = json_decode($requestContent);
