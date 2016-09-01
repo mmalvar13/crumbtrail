82ed96cbf5423ca70753b4239ac0070f63fbe453
Kevin Lee Kirk <klkirkhome@gmail.com>
2016-09-01 16:45:07 -0600

    upper case Trail in use

--- public_html/php/apis/companyActivation/index.php
+++ public_html/php/apis/companyActivation/index.php
@@ -15,7 +15,7 @@ require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once(dirname(__DIR__, 4) . "/vendor/autoload.php");
 
-use Edu\Cnm\Crumbtrail\{
+use Edu\Cnm\CrumbTrail\{
 	Company
 };
 
