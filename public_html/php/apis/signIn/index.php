320c88fb42fd099b800e84421cfcfc359007159b
Monica Alvarez <mmalvar13@gmail.com>
2016-09-02 08:27:02 -0600

    capital T and added a require once in Sign Up API https://app.asana.com/0/161555263951748/169698496837349

--- public_html/php/apis/signIn/index.php
+++ public_html/php/apis/signIn/index.php
@@ -2,9 +2,9 @@
 
 require_once (dirname(__DIR__, 2) ."/classes/autoload.php");
 require_once (dirname(__DIR__,2) . "/lib/xsrf.php");
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php"); //need to add files for this stuff
-
-use Edu\Cnm\Crumbtrail\{Profile};
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once (dirname(__DIR__,4) . "/vendor/autoload.php");
+use Edu\Cnm\CrumbTrail\{Profile};
 
 /**
  * api for sign in
