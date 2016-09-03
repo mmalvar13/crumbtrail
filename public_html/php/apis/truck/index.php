e3de313432e3ec1b2f27be6c511404fd8dc75396
Monica Alvarez <mmalvar13@gmail.com>
2016-09-03 11:47:07 -0600

    added require once for authorization https://app.asana.com/0/161555263951748/169698496837361

--- public_html/php/apis/truck/index.php
+++ public_html/php/apis/truck/index.php
@@ -4,6 +4,7 @@ require_once(dirname(__DIR__, 2) . "/classes/autoload.php");//need to add these
 require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once (dirname(__DIR__,4) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 2) . "/lib/authorization.php");
 
 
 use Edu\Cnm\CrumbTrail\{
