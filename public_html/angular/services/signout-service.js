4d3a2278b2b37e0e92b07239f1b580f0d875841e
Monica Alvarez <mmalvar13@gmail.com>
2016-09-08 19:02:04 -0600

    added signout service. actually added signup service last commit.

--- public_html/angular/services/signout-service.js
+++ public_html/angular/services/signout-service.js
@@ -1,3 +1,7 @@
-/**
- * Created by mmalv on 9/8/2016.
- */
+app.service("SignoutService", function($http){
+	this.SIGNOUT_ENDPOINT = "../../angular/controllers/signout-controller.php";
+
+	this.signout = function(){
+		return($http.get(this.SIGNOUT_ENDPOINT));
+	}
+});
