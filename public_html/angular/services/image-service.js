3ed65802fbd6e00a3ec28c52a0f3418007dbad82
Monica Alvarez <mmalvar13@gmail.com>
2016-09-09 14:14:06 -0600

    image-service created

--- public_html/angular/services/image-service.js
+++ public_html/angular/services/image-service.js
@@ -1,3 +1,41 @@
-/**
- * Created by mmalv on 9/8/2016.
- */
+/*
+ * service for image api
+ * */
+
+app.constant("IMAGE_ENDPOINT", "php/apis/image/");
+app.service("ImageService", function($http, IMAGE_ENDPOINT) {
+
+	function getUrl() {
+		return (IMAGE_ENDPOINT);
+	}
+
+	function getUrlForId(imageId) {
+		return (getUrl() + imageId);
+	}
+
+	this.all = function() {
+		return ($http.get(getUrl()));
+	};
+
+	this.fetch = function(imageId) {
+		return ($http.get(getUrlForId(imageId)));
+	};
+
+	this.fetchImageByImageCompanyId = function(imageCompanyId) {
+		return ($http.get(getUrl() + "?imageCompanyId=" + imageCompanyId));
+	};
+
+	this.fetchImageByImageFileName = function(imageFileName) {
+		return ($http.get(getUrl() + "?imageFileName=" + imageFileName));
+	};
+
+	this.createImage = function(image) {
+		return ($http.post(getUrl(), image));
+	};
+
+	this.deleteImage = function(imageId) {
+		return ($http.delete(getUrlForId(imageId)));
+	};
+
+
+});
