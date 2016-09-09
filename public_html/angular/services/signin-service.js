c9f05671a2d8c066a47c2e285964a92382e18ffc
Monica Alvarez <mmalvar13@gmail.com>
2016-09-08 18:49:35 -0600

    added signin service

--- public_html/angular/services/signin-service.js
+++ public_html/angular/services/signin-service.js
@@ -4,4 +4,12 @@
 
 
 app.constant("SIGNIN_ENDPOINT", "php/apis/signIn/");
-app.service("SignInService", function($http, SIGNIN_ENDPOINT))
+app.service("SignInService", function($http, SIGNIN_ENDPOINT){
+	function getUrl(){
+		return(SIGNIN_ENDPOINT);
+	}
+	this.signin = function(signin){
+		console.log("inside signin service"); //what is this? i am putting this because i saw an example in brew crew.
+		return($http.post(getUrl(), signin));
+	};
+});
