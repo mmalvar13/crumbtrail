b69a6eb044e0bd8765894558f9001f80dc48e689
Monica Alvarez <mmalvar13@gmail.com>
2016-09-09 14:00:29 -0600

    profile-service created

--- public_html/angular/services/profile-service.js
+++ public_html/angular/services/profile-service.js
@@ -1,3 +1,30 @@
-/**
- * Created by mmalv on 9/8/2016.
- */
+/*
+* service for profile api
+* */
+
+app.constant("PROFILE_ENDPOINT", "php/apis/profile/");
+app.service("ProfileService", function($http, PROFILE_ENDPOINT){
+
+	function getUrl(){
+		return(PROFILE_ENDPOINT);
+	}
+
+	function getUrlId(profileId){
+		return(getUrl() + profileId);
+	}
+
+	//why is this just function() with no parameters? what function is it?
+	this.all = function(){
+		return($http.get(getUrl()));
+	};
+
+	this.fetch = function(profileId){
+		return(http.get(getUrlForId(profileId)));
+	};
+
+	this.updateProfile = function(profileId, profile){
+		return($http.put(getUrlForId(profileId, profile)));
+	};
+
+
+});
