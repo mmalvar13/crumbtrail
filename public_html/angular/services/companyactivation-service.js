ae052108aa3d10f21a3dbb1d83745783e24e6c1e
Monica Alvarez <mmalvar13@gmail.com>
2016-09-09 14:44:58 -0600

    companyactivation-service created

--- public_html/angular/services/companyactivation-service.js
+++ public_html/angular/services/companyactivation-service.js
@@ -1,3 +1,29 @@
-/**
- * Created by mmalv on 9/8/2016.
- */
+/*
+* service for company activation API
+* */
+
+app.constant("COMPANYACTIVATION_ENDPOINT", "php/apis/companyActivation/");
+app.service("CompanyactivationService", function($http, COMPANYACTIVATION_ENDPOINT){
+
+	function getUrl(){
+		return(COMPANYACTIVATION_ENDPOINT);
+	}
+
+	function getUrlForId(companyId){
+		return(getUrl() + companyId);
+	} //what do we need here?
+
+
+	this.fetch = function(companyId){
+		return($http.get(getUrlForId(companyId)));
+	}; //is this correct?
+
+	this.fetchCompanyByCompanyActivationToken = function(companyActivationToken){
+		return($http.get(getUrl() + "?companyActivationToken=" + companyActivationToken));
+	};
+
+	//what about company approved??
+
+
+
+});
