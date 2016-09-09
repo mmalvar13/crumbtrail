e08c99adcc27628e5fec68cc564ab9fdd8ee924d
Monica Alvarez <mmalvar13@gmail.com>
2016-09-08 21:55:13 -0600

    wrote signin-controller

--- public_html/angular/controllers/truck-signin-controller.js
+++ public_html/angular/controllers/truck-signin-controller.js
@@ -1,4 +1,32 @@
-app.controller("truckSignInController", ["$scope", function($scope){
+app.controller("truckSignInController", ["$scope", "signinService", function($scope, SigninService){
+	$scope.alerts = [];
 
 
-});
+	/*
+	* creates a sign in?? sends it to sign in api
+	*
+	* @param validated true if Angular validated the form, false if not
+	*
+	* references this.signin function in signin-service
+	* */
+	$scope.signin = function(signInData, validated){ //references this.signin function in signin-service. signIn or signInData? does it matter?
+		console.log("inside signincontroller signin");
+		console.log(signInData); //what is this for?
+		if(validated === true){
+			SigninService.signin(signInData)//should this be signInData?
+				.then(function(result){
+					if(result.data.status === 200){
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+						console.log("good status");
+						$window.location.href = "user/"
+					}else{
+						console.log("bad status");
+						console.log(result.data);
+						$scope.alerts[0] = {type:"danger", msg:result.data.message};
+					}
+				});
+		}
+	};
+
+
+}]);
