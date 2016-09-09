f3338a52a15b201fa4e6e03a8ecb401ece2018e0
Monica Alvarez <mmalvar13@gmail.com>
2016-09-09 13:53:17 -0600

    event-service created

--- public_html/angular/services/event-service.js
+++ public_html/angular/services/event-service.js
@@ -1,33 +1,41 @@
 app.constant("EVENT_ENDPOINT", "php/apis/event/");
-app.service("eventService", function(http, EVENT_ENDPOINT) {
+app.service("EventService", function($http, EVENT_ENDPOINT) {
+
 	function getUrl() {
-		return("Event");
+		return (EVENT_ENDPOINT);
 	}
-	function getUrl() {
-		return(EVENT_ENDPOINT);
-	}
+
 	function getUrlForId(eventId) {
-		return(getUrl() + eventId);
+		return (getUrl() + eventId);
 	}
+
 	this.all = function() {
-		return($http.(getUrl()));
+		return ($http.get(getUrl()));
 	};
-	this.getEventById = function(eventId) {
-		return($http.get(getUrlForId(eventId)));
+
+	this.fetch = function(eventId) {
+		return ($http.get(getUrlForId(eventId)));
 	};
-	this.getEventByTruckId = function(eventTruckId) {
-		return($http.get(getUrl() + "?eventTruckId=" + eventTruckId));
+
+	this.fetchEventByTruckId = function(eventTruckId) {
+		return ($http.get(getUrl() + "?eventTruckId=" + eventTruckId));
 	};
-	this.getEventByStart = function(eventStart) {
-		return($http.get(getUrl() + "?eventStart=" + eventStart));
+
+	this.fetchEventByEventStart = function(eventStart) {
+		return ($http.get(getUrl() + "?eventStart=" + eventStart));
 	};
-	this.getEventByEnd = function(eventEnd) {
-		return($http.get(getUrl() + "?eventEnd=" + eventEnd));
+
+	this.fetchEventByEventEnd = function(eventEnd) {
+		return ($http.get(getUrl() + "?eventEnd=" + eventEnd));
 	};
-	this.eventUpdate = function(eventId, event) {
-		return($http.put(getUrlForId(eventId, event)));
+
+
+	this.updateEvent = function(eventId, event) {
+		return ($http.put(getUrlForId(eventId, event)));
 	};
-	this.eventCreate = function(eventId, event) {
-		return($http.post(getUrl(), event));
+
+	this.createEvent = function(event) {
+		return ($http.post(getUrl(), event));
 	};
-	});
+
+});
