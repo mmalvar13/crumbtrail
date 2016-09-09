8cd6e4ff67c9c45f87ad8e42e8fc315be368097f
Monica Alvarez <mmalvar13@gmail.com>
2016-09-08 18:59:58 -0600

    added signout service

--- public_html/angular/services/signin-service.js
+++ public_html/angular/services/signin-service.js
@@ -8,7 +8,7 @@ app.service("SignInService", function($http, SIGNIN_ENDPOINT){
 	function getUrl(){
 		return(SIGNIN_ENDPOINT);
 	}
-	this.signin = function(signin){
+	this.signin = function(signin){ //why is this this.sign in and signup is this.create??
 		console.log("inside signin service"); //what is this? i am putting this because i saw an example in brew crew.
 		return($http.post(getUrl(), signin));
 	};
--- public_html/angular/services/signup-service.js
+++ public_html/angular/services/signup-service.js
@@ -1,3 +1,14 @@
-/**
- * Created by mmalv on 9/8/2016.
- */
+//service for sign up api
+//@author Monica Alvarez
+
+
+app.constant("SIGNUP_ENDPOINT", "php/apis/signUp/");
+app.service("SignupService", function($http, SIGNUP_ENDPOINT){
+	function getUrl(){
+		return(SIGNUP_ENDPOINT);
+	}
+	this.create = function(signup){
+		console.log("inside signup service");
+		return($http.post(getUrl(),signup));
+	};
+});
