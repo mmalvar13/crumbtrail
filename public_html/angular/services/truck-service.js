f7fcf9a6a8ff51106988dbea1d46e9c284629148
Monica Alvarez <mmalvar13@gmail.com>
2016-09-08 20:31:19 -0600

    added documentation and comments to truck service

--- public_html/angular/services/truck-service.js
+++ public_html/angular/services/truck-service.js
@@ -1,3 +1,86 @@
-/**
- * Created by mmalv on 9/8/2016.
- */
+/*
+* service for truck api
+*
+* @author Monica Alvarez
+* */
+
+/*define a constant that holds the base url of the Truck API
+* This has the endpoint "php/apis/truck".
+*
+* This service will build a URL, access it, and grab whatever data it can.
+*
+* pass whatever ID or object needed in order to complete the operation.
+*
+* You will call this service's methods within the Truck Controller
+* */
+app.constant("TRUCK_ENDPOINT", "php/apis/truck/");
+
+/*
+* Initializes the Truck Service
+*
+* We inject the $http service. we use this to make REST calls
+* We also inject our endpoint so that it is usable inside the service.
+* */
+app.service("TruckService", function($http, TRUCK_ENDPOINT){
+
+	/*
+	 * Returns the truck endpoint for use in other methods
+	 * */
+
+	function getUrl(){
+		return(TRUCK_ENDPOINT);
+	}
+
+/*
+* Builds a URL for getting a truck by ID (example: "apis/truck/3")
+* */
+	function getUrlForId(truckId){
+		return(getUrl() + truckId);
+	}
+
+	/*
+	* GETS all trucks
+	* */
+	this.all = function(){
+		return($http.get(getUrl()));
+	};
+
+
+	/*
+	* GETS a truck by ID
+	* */
+	this.fetch = function(truckId){
+		return($http.get(getUrlForId(truckId)));
+	};
+
+
+	/*
+	* GETS a truck by truckCompanyId
+	* */
+	this.fetchTruckByTruckCompanyId = function(truckCompanyId){
+		return($http.get(getUrl() + "?truckCompanyId=" + truckCompanyId));
+	};
+
+	/*
+	* POSTS a truck (creates one)
+	* */
+	this.createTruck = function(truck){
+		return($http.post(getUrl(), truck));
+	};
+
+
+	/*
+	* PUTS a truck
+	* */
+	this.updateTruck = function(truckId, truck){
+		return($http.put(getUrlForId(truckId), truck));
+	};
+
+
+	/*
+	* DELETES a truck
+	* */
+	this.deleteTruck = function(truckId){
+		return($http.delete(getUrlForId(truckId)));
+	};
+});
