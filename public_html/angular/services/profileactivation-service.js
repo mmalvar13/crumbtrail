03e72426df724a52080999663c7a3e8c764c81da
Monica Alvarez <mmalvar13@gmail.com>
2016-09-09 15:01:37 -0600

    profileactivation-service created

--- public_html/angular/services/profileactivation-service.js
+++ public_html/angular/services/profileactivation-service.js
@@ -6,11 +6,26 @@
  **/
 
 app.constant("PROFILEACTIVATION_ENDPOINT", "php/apis/profileActivation/");
-app.service("ProfileactivationService", function($http, PROFILEACTIVATION_ENDPOINT){
-	function getUrl(){
-		return(PROFILEACTIVATION_ENDPOINT);
+app.service("ProfileactivationService", function($http, PROFILEACTIVATION_ENDPOINT) {
+	function getUrl() {
+		return (PROFILEACTIVATION_ENDPOINT);
 	}
-	this.fetch = function(misquoteId){
-		return($http.get(getUrlForId(misquoteId)));
+
+	function getUrlForId(profileId) {
+		return (getUrl() + profileId); //Is this supposed to be profile id?? what should it be.
 	}
-})
+
+	this.fetchProfileByProfileByProfileActivationToken = function(profileActivationToken) {
+		return ($http.get(getUrl() + "?profileActivationToken=" + profileActivationToken));
+	};
+
+	this.fetchEmployByEmployCompanyIdAndEmployProfileId = function(employCompanyId, employProfileId) {
+		return ($http.get(getUrl() + "?employCompanyId=" + employCompanyId, "?employProfileId=" + employProfileId));
+	};
+
+	this.fetchCompanyByCompanyId = function(companyId) {
+		return ($http.get(getUrl() + "?companyId=" + companyId));
+	};
+
+
+});
