33d4f088b2a56687cec2abdebfbb67bbfb5606cc
Kevin Lee Kirk <klkirkhome@gmail.com>
2016-09-10 08:46:38 -0600

    finished prototype of profile-controller.js

--- public_html/angular/controllers/profile-controller.js
+++ public_html/angular/controllers/profile-controller.js
@@ -10,12 +10,13 @@ app.controller('ProfileController', ["$scope", "CompanyService", "EventService",
 
 	$scope.alerts = [];
 
-	/*------------------------- CompanyService methods --------------------------------------------------------*/
+	/* ---------------------- Read stuff: getFooByBars -------------------------------------------------------- */
+	/* ------------------------- CompanyService methods ------------------------------------------------------ */
 	$scope.getCompanyByCompanyId = function(companyId) {
 		CompanyService.fetchCompanyById(companyId)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
@@ -26,7 +27,7 @@ app.controller('ProfileController', ["$scope", "CompanyService", "EventService",
 		CompanyService.fetchCompanyByCompanyName(companyName)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
@@ -37,7 +38,7 @@ app.controller('ProfileController', ["$scope", "CompanyService", "EventService",
 		CompanyService.fetchCompanyByCompanyMenuText(companyMenuText)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
@@ -48,19 +49,19 @@ app.controller('ProfileController', ["$scope", "CompanyService", "EventService",
 		CompanyService.fetchCompanyByCompanyDescription(companyDescription)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
 			})
 	};
 
-	/*---------------- EventService methods ------------------------*/
+	/*-------------------- EventService methods ----------------------------*/
 	$scope.getEventByEventId = function(eventId) {
 		EventService.fetchEventByEventId(eventId)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
@@ -71,7 +72,7 @@ app.controller('ProfileController', ["$scope", "CompanyService", "EventService",
 		EventService.fetchEventByEventEndAndEventStart(eventEnd, eventStart)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
@@ -82,7 +83,7 @@ app.controller('ProfileController', ["$scope", "CompanyService", "EventService",
 		EventService.fetchEventByEventTruckId(eventTruckId)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
@@ -93,45 +94,156 @@ app.controller('ProfileController', ["$scope", "CompanyService", "EventService",
 		EventService.fetchEventByEventIdAndEventTruckId(eventTruckId)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
 			})
 	};
 
-	/*------------------------------------ ImageService Methods --------------------------------------*/
+	/*--------------------------- ImageService Methods --------------------------------------*/
+		$scope.getImageByImageId = function(imageId) {
+			ImageService.fetchImageByImageId(imageId)
+				.then(function(result) {
+					if(result.status.data === 200) {
+						$scope.profileData = result.data.data;
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				})
+		};
 
+		$scope.getImageByImageCompanyId = function(imageCompanyId) {
+			ImageService.fetchImageByImageCompanyId(imageCompanyId)
+				.then(function(result) {
+					if(result.status.data === 200) {
+						$scope.profileData = result.data.data;
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				})
+		};
 
+		$scope.getImageByImageFileName = function(imageFileName) {
+			ImageService.fetchImageByImageFileName(imageFileName)
+				.then(function(result) {
+					if(result.status.data === 200) {
+						$scope.profileData = result.data.data;
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				})
+		};
 
-
-	/*------------------------------------ ProfileService Methods --------------------------------------*/
+	/*-------------------------- ProfileService Methods --------------------------------------*/
 	$scope.getProfileByProfileId = function(profileId) {
 		EventService.fetchProfileByProfileId(profileId)
 			.then(function(result) {
 				if(result.status.data === 200) {
-					$scope.mapData = result.data.data;
+					$scope.profileData = result.data.data;
 				} else {
 					$scope.alerts[0] = {type: "danger", msg: result.data.message};
 				}
 			})
 	};
 
+	/* ---------------------- Create, Update, Delete stuff ------------------------------------- */
+	/* --------------------------- Create ------------------------------------------------------ */
+		$scope.profileCreate = function(profile, validated) {
+			if(validated === true) {
+				CompanyService.create(company)			// TODO  Or should this be (profile) ???
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+				EventService.create(event)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+				ImageService.create(image)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+				ProfileService.create(profile)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+			}
+		};
 
+		/* --------------------------- Update ------------------------------------------------- */
+		$scope.profileUpdate = function(profile, validated) {
+			if(validated === true) {
+				CompanyService.update(profile)			// TODO  Or should this be (company) ???
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+				EventService.update(profile)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+				ImageService.update(profile)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+				ProfileService.update(profile)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+			}
+		};
 
+		/* -------------------------- Delete -------------------------------------------- */
+		$scope.delete = function(map, validated) {
+			if(validated === true) {
+				TruckService.delete(profile)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+				CompanyService.delete(profile)
+					.then(function(result) {
+						if(result.data.status === 200) {
+							$scope.alerts[0] = {type: "success", msg: result.data.message};
+						} else {
+							$scope.alerts[0] = {type: "danger", msg: result.data.message};
+						}
+					});
+			}
+		};
 
-
-	/*------------------------------------ CRUD .... !!!  --------------------------------------*/
-
-
-
-
-
-
-
-
-
-
-
-
-}]);
+	}]);
