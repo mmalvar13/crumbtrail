899f5b0c4ff4a216a5d84221e2fa79f9dc4c8970
Monica Alvarez <mmalvar13@gmail.com>
2016-09-10 12:14:00 -0600

    added angular form stuff to sign in view

--- public_html/angular/views/signin-view.php
+++ public_html/angular/views/signin-view.php
@@ -1,39 +1,40 @@
-
-
-			<!--------------------------------MAIN BODY----------------------------------->
-			<div class="container">
-				<div class="row">
-					<h1 class="text-center">Food Truck Login</h1>
-					<hr class="truck-login-hr">
-				</div>
-				<!-----------LOGIN FORM------------------->
-				<div class="col-md-4 col-md-offset-4">
-					<form>
-						<label for="inputEmail" class="sr-only">Email address</label>
-						<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required=""
-								 autofocus="">
-						<label for="inputPassword" class="sr-only">Password</label>
-						<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-						<div class="checkbox">
-							<label>
-								<input type="checkbox" value="remember-me"> Remember me
-							</label>
-						</div>
-						<button class="btn btn-lg btn-warning btn-block" type="submit">Sign in</button>
-					</form>
-				</div>
-
-
-				<!----------NEED TO REGISTER SECTION-------->
-				<div class="row">
-					<div class="col-md-8 col-md-offset-2">
-						<button type="button" class="btn btn-outline-warning btn-lg btn-block truck-login-register-btn">Need
-							to register your company?
-						</button>
-					</div>
-				</div>
+<!--------------------------------MAIN BODY----------------------------------->
+<div class="container">
+	<div class="row">
+		<h1 class="text-center">Food Truck Login</h1>
+		<hr class="truck-login-hr">
+	</div>
+	<!-----------LOGIN FORM------------------->
+	<div class="col-md-4 col-md-offset-4">
+		<form name="signinForm" id="signinForm" class="form-horizontal well" ng-controller="SigninFormController"
+				ng-submit="submit(formData, signinForm.$valid);" novalidate>
+			<div class="form-group" ng-class="{'has-error':signinForm.inputEmail.$touched && signinForm.inputEmail.$invalid}">
+				<label for="inputEmail" class="sr-only">Email address</label>
+				<input type="email" id="inputEmail" name="inputEmail" class="form-control" ng-model="formData.inputEmail" placeholder="Email address" required="true" autofocus=""/>
 			</div>
-		</div> <!------sfooter------>
+			<div class
+			<label for="inputPassword" class="sr-only">Password</label>
+			<input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+			<div class="checkbox">
+				<label>
+					<input type="checkbox" value="remember-me"> Remember me
+				</label>
+			</div>
+			<button class="btn btn-lg btn-warning btn-block" type="submit">Sign in</button>
+		</form>
+	</div>
+
+
+	<!----------NEED TO REGISTER SECTION-------->
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<button type="button" class="btn btn-outline-warning btn-lg btn-block truck-login-register-btn">Need
+				to register your company?
+			</button>
+		</div>
+	</div>
+</div>
+</div> <!------sfooter------>
 
 
 
