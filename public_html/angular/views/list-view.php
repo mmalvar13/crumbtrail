4c5f6bd21d04f943b13cdd3d0046371c2a66c674
baca.loren@gmail.com <baca.loren@gmail.com>
2016-09-09 23:36:00 -0600

    fixed copy pasta and container error in list view

--- public_html/angular/views/list-view.php
+++ public_html/angular/views/list-view.php
@@ -1,20 +1,22 @@
 
 
 			<!----MAIN BODY --->
-			<div class="container">
-				<div class="row">
-						<div class="truck-list-box truck-image">
-							<div class="truck-list-serving-icon"></div>
-							<h3 class="truck-h3">Company Name</h3>
-							<hr class="truck-list-hr">
-							<ul class="truck-list-ul">
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-							</ul>
-						</div>
+			<div class="row">
+				<div class="container">
+					<div class="truck-list-box truck-image">
+						<div class="truck-list-serving-icon"></div>
+						<h3 class="truck-h3">Company Name</h3>
+						<hr class="truck-list-hr">
+						<p class="truck-list-text">We make the best damn tacos in the whole damn world!</p>
+						<ul class="truck-list-ul">
+							<li>Tacos</li>
+							<li>Burritos</li>
+							<li>Nachos</li>
+							<li>People-meat</li>
+						</ul>
+					</div>
 				</div>
+			</div>
 
 				<div class="row">
 					<div class="container">
