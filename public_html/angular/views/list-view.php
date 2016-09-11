b36b5ca927641e697b30c267d71017d661ce3152
Monica Alvarez <mmalvar13@gmail.com>
2016-09-11 17:36:14 -0600

    added ng-repeat to list-view

--- public_html/angular/views/list-view.php
+++ public_html/angular/views/list-view.php
@@ -1,69 +1,67 @@
+<!----MAIN BODY --->
+<div class="row">
+	<div class="container" ng-repeat="company in companyData">
+		<div class="truck-list-box truck-image">
+			<div class="truck-list-serving-icon"></div>
+			<h3 class="truck-h3">{{company.companyName}}</h3>
+			<hr class="truck-list-hr">
+			<p class="truck-list-text">{{company.companyDescription}}</p>
+			<ul class="truck-list-ul">
+				<li>Tacos</li>
+				<li>Burritos</li>
+				<li>Nachos</li>
+				<li>People-meat</li>
+			</ul>
+		</div>
+	</div>
+</div>
 
+<div class="row">
+	<div class="container">
+		<div class="truck-list-box truck-image">
+			<div class="truck-list-serving-icon"></div>
+			<h3 class="truck-h3">Company Name</h3>
+			<hr class="truck-list-hr">
+			<p class="truck-list-text">We make the best damn tacos in the whole damn world!</p>
+			<ul class="truck-list-ul">
+				<li>Tacos</li>
+				<li>Burritos</li>
+				<li>Nachos</li>
+				<li>People-meat</li>
+			</ul>
+		</div>
+	</div>
+</div>
 
-			<!----MAIN BODY --->
-			<div class="row">
-				<div class="container">
-					<div class="truck-list-box truck-image">
-						<div class="truck-list-serving-icon"></div>
-						<h3 class="truck-h3">{{companyData:companyName}}</h3>
-						<hr class="truck-list-hr">
-						<p class="truck-list-text">We make the best damn tacos in the whole damn world!</p>
-						<ul class="truck-list-ul">
-							<li>Tacos</li>
-							<li>Burritos</li>
-							<li>Nachos</li>
-							<li>People-meat</li>
-						</ul>
-					</div>
-				</div>
-			</div>
+<div class="row">
+	<div class="container">
+		<div class="truck-list-box truck-image">
+			<div class="truck-list-serving-icon"></div>
+			<h3 class="truck-h3">Company Name</h3>
+			<hr class="truck-list-hr">
+			<ul class="truck-list-ul">
+				<li>asdfasdfasdf</li>
+				<li>asdfasdfasdf</li>
+				<li>asdfasdfasdf</li>
+				<li>asdfasdfasdf</li>
+			</ul>
+		</div>
+	</div>
+</div>
 
-				<div class="row">
-					<div class="container">
-						<div class="truck-list-box truck-image">
-							<div class="truck-list-serving-icon"></div>
-							<h3 class="truck-h3">Company Name</h3>
-							<hr class="truck-list-hr">
-							<p class="truck-list-text">We make the best damn tacos in the whole damn world!</p>
-							<ul class="truck-list-ul">
-								<li>Tacos</li>
-								<li>Burritos</li>
-								<li>Nachos</li>
-								<li>People-meat</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="container">
-						<div class="truck-list-box truck-image">
-							<div class="truck-list-serving-icon"></div>
-							<h3 class="truck-h3">Company Name</h3>
-							<hr class="truck-list-hr">
-							<ul class="truck-list-ul">
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-
-				<div class="row">
-					<div class="container">
-						<div class="truck-list-box truck-image">
-							<div class="truck-list-serving-icon"></div>
-							<h3 class="truck-h3">Company Name</h3>
-							<hr class="truck-list-hr">
-							<ul class="truck-list-ul">
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-								<li>asdfasdfasdf</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
+<div class="row">
+	<div class="container">
+		<div class="truck-list-box truck-image">
+			<div class="truck-list-serving-icon"></div>
+			<h3 class="truck-h3">Company Name</h3>
+			<hr class="truck-list-hr">
+			<ul class="truck-list-ul">
+				<li>asdfasdfasdf</li>
+				<li>asdfasdfasdf</li>
+				<li>asdfasdfasdf</li>
+				<li>asdfasdfasdf</li>
+			</ul>
+		</div>
+	</div>
+</div>
+</div>
