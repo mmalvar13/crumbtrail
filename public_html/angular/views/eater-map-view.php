5f702910e877cf0d19fc85f71f982a777a571c09
Kevin Lee Kirk <klkirkhome@gmail.com>
2016-09-12 13:43:22 -0600

    modified eater-map-view

--- public_html/angular/views/eater-map-view.php
+++ public_html/angular/views/eater-map-view.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-
-<html ng-app="Crumbtrail">
-	<head>
-		<title>Eater Map View</title>
-		<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-		<meta charset="utf-8">
-		<style>
-			html, body {
-				height: 100%;
-				margin: 0;
-				padding: 0;
-			}
-			#map {
-				height: 100%;
-			}
-		</style>
-	</head>
-
-	<body>
 		<div id="map"></div>
 		<script>
 			function initMap() {
@@ -69,32 +49,3 @@
 		<script async defer
 				src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwGxzLKd5waiJFHo5nsZqZl3xJN64JxIc&callback=initMap">
 		</script>
-
-	</body>
-</html>
-
-<!DOCTYPE html>
-<!--<html ng-app="Crumbtrail">-->
-<!--	<head>-->
-<!--		<!--  Is this different, for the Angular google maps? -->-->
-<!--		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwGxzLKd5waiJFHo5nsZqZl3xJN64JxIc&callback=initMap"></script>-->
-<!---->
-<!--		<!-- Need the paths, below: -->-->
-<!--		<script src='/path/to/lodash[.min].js'></script>-->
-<!--		<script src='/path/to/angular[.min].js'></script>-->
-<!--		<script src='/path/to/angular-simple-logger/angular-simple-logger[.min].js'></script>-->
-<!--		<script src='/path/to/angular-google-maps[.min].js'></script>-->
-<!--	</head>-->
-<!--	<body>-->
-<!--		<div id="map_canvas" ng-controller="MapController">-->
-<!---->
-<!--			<!-- The center of the map = hungry eater's current location. -->-->
-<!--			<!-- Does 'map.center' get this from map-controller.js? -->-->
-<!--			<ui-gmap-google-map center='map.center' zoom='map.zoom'></ui-gmap-google-map>-->
-<!---->
-<!--			<!-- "marker.id" = eventId, for each specific event. -->-->
-<!--			<!-- So I'll need an ng-repeat for the ui-gmap-marker thing? -->-->
-<!--			<ui-gmap-marker coords="marker.coords" idkey="marker.id"></ui-gmap-marker>-->
-<!--		</div>-->
-<!--	</body>-->
-<!--</html-->
