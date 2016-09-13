06cab048c964015f669868d18ec4ac9c3cba80cc
Monica Alvarez <mmalvar13@gmail.com>
2016-09-12 18:01:37 -0600

    list view css changes

--- public_html/angular/views/list-view.php
+++ public_html/angular/views/list-view.php
@@ -1,17 +1,35 @@
 <!----MAIN BODY --->
+
 <div class="row">
-	<div class="container" ng-repeat="company in companyData">
-		<div class="truck-list-box truck-image">
-			<div class="truck-list-serving-icon"></div>
-			<h3 class="truck-h3" ><a href="profile/{{company.companyId}}">{{company.companyName}}</a></h3>
-			<hr class="truck-list-hr">
-			<p class="truck-list-text" >{{company.companyDescription}}</p>
-			<ul class="truck-list-ul">
-				<li>{{company.companyMenuText}}</li>
-			</ul>
+	<div class="container">
+		<div class="col-md-6" ng-repeat="company in companyData">
+			<div class="truck-list-box truck-image">
+				<div class="truck-list-serving-icon"></div>
+				<h3 class="truck-h3"><a href="profile/{{company.companyId}}">{{company.companyName}}</a></h3>
+				<hr class="truck-list-hr">
+				<p class="truck-list-text">{{company.companyDescription}}</p>
+				<ul class="truck-list-ul">
+					<li>{{company.companyMenuText}}</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
+
+<!--<div class="row">-->
+<!--	<div class="container" ng-repeat="company in companyData">-->
+<!--		<div class="truck-list-box truck-image">-->
+<!--			<div class="truck-list-serving-icon"></div>-->
+<!--			<h3 class="truck-h3" ><a href="profile/{{company.companyId}}">{{company.companyName}}</a></h3>-->
+<!--			<hr class="truck-list-hr">-->
+<!--			<p class="truck-list-text" >{{company.companyDescription}}</p>-->
+<!--			<ul class="truck-list-ul">-->
+<!--				<li>{{company.companyMenuText}}</li>-->
+<!--			</ul>-->
+<!--		</div>-->
+<!--	</div>-->
+<!--</div>-->
+
 
 <!--<div class="row">-->
 <!--	<div class="container">-->
