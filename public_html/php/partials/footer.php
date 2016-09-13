3f078f8cf18d5895bd6b0d5352c159f76235f1fd
baca.loren@gmail.com <baca.loren@gmail.com>
2016-09-13 10:04:04 -0600

    footer logo?

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -1,10 +1,13 @@
 <footer class="bfoot">
 <!--	<div class="container">-->
 <!--		<div class="row">-->
-<!--		<div class="col-xs-6 col-xs-offset-3">-->
-<!--			<img class="foot-logo" src="images/crumbtraillogo.png">-->
+<!--		<div class="col-xs-6 col-offset-3">-->
+	<div class="foot-logo-div">
+			<a href="#"><img  class="foot-logo" src="images/crumbtraillogo.png"></a>
+	</div>
 <!--		</div>-->
 <!--		</div>-->
 <!--	</div>-->
+
 </footer>
 
