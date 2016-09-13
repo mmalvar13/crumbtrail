4ed5b17107d8bcb987746ccdf00c3126a5bcbfc3
Monica Alvarez <mmalvar13@gmail.com>
2016-09-13 17:51:52 -0600

    added eater map, loren rules

--- public_html/angular/views/eater-map-view.php
+++ public_html/angular/views/eater-map-view.php
@@ -1,3 +1,11 @@
-<ui-gmap-google-map center='map.center' zoom='map.zoom'>
+<!--<ui-gmap-google-map center='map.center' zoom='map.zoom'>-->
+<!--	<ui-gmap-marker coords="map.center"idkey="eaterMarkerId.id"></ui-gmap-marker>-->
+<!--</ui-gmap-google-map>-->
+
+
+<ui-gmap-google-map ng-hide="editing" center='map.center' zoom='map.zoom'>
 	<ui-gmap-marker coords="map.center"idkey="eaterMarkerId.id"></ui-gmap-marker>
+	<ui-gmap-marker ng-repeat="event in activeEvents" coords="{'latitude': event.eventLocation.lat, 'longitude': event.eventLocation.long}" idkey="event.eventId">
+	</ui-gmap-marker>
 </ui-gmap-google-map>
+
