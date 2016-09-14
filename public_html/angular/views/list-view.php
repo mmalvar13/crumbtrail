7bb28a76606d16cdc6383306cdad058a56fb6222
baca.loren@gmail.com <baca.loren@gmail.com>
2016-09-14 10:43:20 -0600

    profile done for now

--- public_html/angular/views/list-view.php
+++ public_html/angular/views/list-view.php
@@ -1,22 +1,25 @@
 <!----MAIN BODY --->
 
 <div class="row">
-	<div class="container-fluid truck-body">
-<!--		<div class="container">-->
-		<h1 id="all-trucks">All Food Trucks</h1>
+	<div class="truck-body">
+		<div class="container">
+			<h1 id="all-trucks">All Food Trucks</h1>
 			<hr class="truck-list-hr">
-		<div class="col-md-6" ng-repeat="company in companyData">
-			<div class="truck-list-box truck-image">
-				<div class="truck-list-serving-icon"></div>
-				<h3 class="truck-h3"><a href="profile/{{company.companyId}}">{{company.companyName}}</a></h3>
-				<hr class="truck-list-hr">
-				<p class="truck-list-text">{{company.companyDescription}}</p>
-				<ul class="truck-list-ul">
-					<li>{{company.companyMenuText}}</li>
-				</ul>
+			<div class="col-md-6" ng-repeat="company in companyData">
+
+				<div class="truck-list-box truck-image">
+					<div class="truck-list-serving-icon"></div>
+					<h3 class="truck-h3"><a href="profile/{{company.companyId}}">{{company.companyName}}</a></h3>
+					<hr class="truck-list-hr">
+					<p class="truck-list-text">{{company.companyDescription}}</p>
+					<ul class="truck-list-ul">
+						<li>{{company.companyMenuText}}</li>
+					</ul>
+
+				</div>
+
 			</div>
 		</div>
-<!--		</div>-->
 	</div>
 </div>
 
