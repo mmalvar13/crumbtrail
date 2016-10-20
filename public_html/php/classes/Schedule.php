5fc96566f79a5dc7cdd2026bd52cb405a96aab24
Victoria Chacon <Vchaco06@gmail.com>
2016-10-20 16:50:48 -0600

    Wrote the DocBlock for Schedule.php https://app.asana.com/0/195124829373790/195124240735389

---
+++ public_html/php/classes/Schedule.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Edu\Cnm\CrumbTrail;
+
+require_once("autoload.php");
+
+//begin Docblock
+/**
+ * Schedule Identifier
+ *
+ * This will be used to identify the different food truck schedules that will be in our database. This will be a weekly schedule with the possibility multiple objects on any given day. This will run Monday through Sunday.
+ *
+ * @author Victoria Chacon <victoriousdesignco@gmail.com>
+ **/
