fafbd26bec1b81af379b73763e0529b27de3b5c1
Loren Baca <baca.loren@gmail.com>
2016-10-22 17:00:02 -0600

    state variables done, working on getters/setters
    https://app.asana.com/0/195124829377891/195124240735393

--- public_html/php/classes/ExtraServing.php
+++ public_html/php/classes/ExtraServing.php
@@ -13,9 +13,9 @@ require_once("autoload.php");
  *
  * Author @L   baca.loren@gmail.com
  */
+class ExtraServing implements \JsonSerializable {
 
-
-class ExtraServing implements \JsonSerializable{
+	use ValidateDate;
 
 	/**
 	 * primary key for ExtraServing Class
@@ -25,10 +25,61 @@ class ExtraServing implements \JsonSerializable{
 
 	/**
 	 * foreign key to Company Class
-	 * @var int $extraServingCompanyId;
+	 * @var int $extraServingCompanyId
 	 */
 	private $extraServingCompanyId;
 
+	/**
+	 * description of the extra serving event
+	 * @var string $extraServingDescription
+	 */
+	private $extraServingDescription;
+
+	/**
+	 * location of where the food truck will be serving
+	 * @var string $extraServingLocation
+	 */
+	private $extraServingLocation;
+
+	/**
+	 * start time of the extra serving event
+	 * @var \DateTime $extraServingStartTime
+	 */
+	private $extraServingStartTime;
+
+	/**
+	 * end time of the extra serving event
+	 * @var \DateTime $extraServingEndTime
+	 */
+	private $extraServingEndTime;
 
 
+	/**
+	 * constructor for extraServing
+	 */
+
+
+//	--------------------------------------SETTERS AND GETTERS SECTION---------------------------------
+
+	/**
+	 * getter for extraServingId
+	 * @return int|null for $extraServingId
+	 */
+
+	public function getExtraServingId() {
+		return ($this->extraServingId);
+
+	}
+
+	/**
+	 * setter for extraServingId
+	 * @param int|null $newExtraServingId
+	 * @throws \InvalidArgumentException if $newExtraServingId not valid
+	 * @throws \RangeException if $newExtraServingId negative or zero
+	 * @throws \TypeError if $newExtraServingId not an int
+	 */
+
+	public function setExtraServingId(int $newExtraServingId = null){
+
+	}
 }
