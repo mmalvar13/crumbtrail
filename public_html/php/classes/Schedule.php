f8b3f151b25aab16fc2ba7684a12d5826854cb40
Victoria Chacon <Vchaco06@gmail.com>
2016-10-26 14:03:20 -0600

    Beginning Insert update and delete methods https://app.asana.com/0/195124829373790/195124240735389

--- public_html/php/classes/Schedule.php
+++ public_html/php/classes/Schedule.php
@@ -14,28 +14,29 @@ require_once("autoload.php");
  **/
 
 /* This is a test...jk it worked....hmmmm */
+
 /* Begin class here */
 
 class Schedule implements \JsonSerializable {
 	/**
 	 * id for this schedule; this is the primary key
-	 * @var int $scheduleId;
+	 * @var int $scheduleId ;
 	 **/
 	private $scheduleId;
 	/**
 	 * id for this schedule's connection to its company
-	 * @var int $scheduleCompanyId;
+	 * @var int $scheduleCompanyId ;
 	 */
 	private $scheduleCompanyId;
 	/**
 	 * id for this schedule's day of the week (Todo: Does this need to be private? So someone cant mess around with a trucks schedule right? (down))
-	 * @var string $scheduleDayOfWeek;
+	 * @var string $scheduleDayOfWeek ;
 	 */
 	private $scheduleDayOfWeek;
 	/**
 	 * id for this schedule's start time of operation
 	 *
-	 * @var \DateTime $scheduleStartTime;
+	 * @var \DateTime $scheduleStartTime ;
 	 */
 	private $scheduleStartTime;
 	/**
@@ -46,7 +47,7 @@ class Schedule implements \JsonSerializable {
 	private $scheduleEndTime;
 	/**
 	 * id for this schedule's location name
-	 * @var string $scheduleLocationName;
+	 * @var string $scheduleLocationName ;
 	 */
 	private $scheduleLocationName;
 	/**
@@ -66,37 +67,37 @@ class Schedule implements \JsonSerializable {
 	 * @param \DateTime|string|null $newScheduleStartTime data and time of the schedule (taken from the event class)
 	 * @param \DateTime|string|null $newScheduleEndTime data and time of the schedule (taken from the event class)
 	 *
-	Todo: @param string $newScheduleTime string of times set for an event (if we deiced to make this a string)
+	 * Todo: @param string $newScheduleTime string of times set for an event (if we deiced to make this a string)
 	 * @throws \RangeException when the integer is negative or strings are too long
 	 * @throws \TypeError if the the days of the week do not match our parameters
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \Exception when error need to be called in the code??
 	 **/
 
-public function __construct(int $newScheduleId = null, int $newScheduleCompanyId, string $newScheduleDayOfWeek, string $newScheduleLocationName, string $newScheduleLocationAddress, $newScheduleStartTime = null, $newScheduleEndTime = null) {
-	try {
-		$this->setScheduleId($newScheduleId);
-		$this->setScheduleCompanyId($newScheduleCompanyId);
-		$this->setScheduleDayOfWeek($newScheduleDayOfWeek);
-		$this->setScheduleLocationName($newScheduleLocationName);
-		$this->setScheduleLocationAddress($newScheduleLocationAddress);
-		$this->setScheduleStartTime($newScheduleStartTime);
-		$this->setScheduleEndTime($newScheduleEndTime);
-	} catch(\RangeException $range) {
-		throw(new \RangeException($range->getMessage(), 0, $range));
-	} catch(\InvalidArgumentException $invalidArgument) {
-		throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
-	} catch(\Exception $exception) {
-		throw(new \Exception($exception->getMessage(), 0, $exception));
+	public function __construct(int $newScheduleId = null, int $newScheduleCompanyId, string $newScheduleDayOfWeek, string $newScheduleLocationName, string $newScheduleLocationAddress, $newScheduleStartTime = null, $newScheduleEndTime = null) {
+		try {
+			$this->setScheduleId($newScheduleId);
+			$this->setScheduleCompanyId($newScheduleCompanyId);
+			$this->setScheduleDayOfWeek($newScheduleDayOfWeek);
+			$this->setScheduleLocationName($newScheduleLocationName);
+			$this->setScheduleLocationAddress($newScheduleLocationAddress);
+			$this->setScheduleStartTime($newScheduleStartTime);
+			$this->setScheduleEndTime($newScheduleEndTime);
+		} catch(\RangeException $range) {
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\Exception $exception) {
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
 	}
-}
-/* Begin the setters and getters Here...accessors and mutators */
+	/* Begin the setters and getters Here...accessors and mutators */
 
 	/**
- 	* accessor for the scheduleId
- 	*
- 	* @return int|null value of scheduleId
- 	**/
+	 * accessor for the scheduleId
+	 *
+	 * @return int|null value of scheduleId
+	 **/
 
 	public function getScheduleId() {
 		return ($this->scheduleId);
@@ -114,12 +115,13 @@ public function __construct(int $newScheduleId = null, int $newScheduleCompanyId
 			return;
 		}
 		//verifying whether the id is positive
-		if($newScheduleId <=0) {
-			throw( new \RangeException("This schedule id is not positive"));
+		if($newScheduleId <= 0) {
+			throw(new \RangeException("This schedule id is not positive"));
 		}
 		//convert and store schedule
 		$this->scheduleId = $newScheduleId;
 	}
+
 	/**
 	 * accessor for the scheduleCompanyId
 	 *
@@ -128,6 +130,7 @@ public function __construct(int $newScheduleId = null, int $newScheduleCompanyId
 	public function getScheduleCompanyId() {
 		return ($this->scheduleCompanyId);
 	}
+
 	/**
 	 * mutator method for scheduleCompanyId
 	 * @param int|null value for scheduleCompanyId
@@ -139,12 +142,13 @@ public function __construct(int $newScheduleId = null, int $newScheduleCompanyId
 			$this->scheduleCompanyId = null;
 			return;
 		}
-		if ($newScheduleCompanyId <=0) {
-			throw( new \RangeException("This schedule company id is not positive"));
+		if($newScheduleCompanyId <= 0) {
+			throw(new \RangeException("This schedule company id is not positive"));
 		}
 		//convert and store this schedule
 		$this->scheduleCompanyId = $newScheduleCompanyId;
 	}
+
 	/**
 	 * accessor for the scheduleDayOfWeek
 	 *
@@ -153,6 +157,7 @@ public function __construct(int $newScheduleId = null, int $newScheduleCompanyId
 	public function getScheduleDayOfWeek() {
 		return ($this->scheduleDayOfWeek);
 	}
+
 	/**
 	 * mutator method for scheduleDayOfWeek
 	 * @param string $newScheduleDayOfWeek the string day of the week
@@ -166,21 +171,22 @@ public function __construct(int $newScheduleId = null, int $newScheduleCompanyId
 		}
 		/* make sure the scheduled day is no more than nine characters */
 		/* is this right? */
-		if ($newScheduleDayOfWeek !== "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday" or "Saturday" or "Sunday") {
-			throw( new \TypeError("This day of the week does not exist"));
+		if($newScheduleDayOfWeek !== "Monday" or "Tuesday" or "Wednesday" or "Thursday" or "Friday" or "Saturday" or "Sunday") {
+			throw(new \TypeError("This day of the week does not exist"));
 		}
 		//convert and store this day of the week
 		$this->scheduleDayOfWeek = $newScheduleDayOfWeek;
 	}
 	/**
-	 *accessor for the schedule location
+	 *accessor for the schedule location name
 	 *
 	 * @return string $scheduleLocationName the string name of the location
 	 **/
-/* example for @return (CNM)*/
+	/* example for @return (CNM)*/
 	public function getScheduleLocationName() {
 		return ($this->scheduleLocationName);
 	}
+
 	/**
 	 * mutator method for scheduleLocationName
 	 * @param string $newScheduleLocationName the string of the location name
@@ -196,5 +202,95 @@ public function __construct(int $newScheduleId = null, int $newScheduleCompanyId
 		}
 		$this->scheduleLocationName = $newScheduleLocationName;
 	}
+	/**
+	 * accessor for schedule address
+	 *
+	 * @return string $scheduleLocationAddress the string of the address
+	 */
+	/* example for above 1232 cherry ln sw...etc*/
+	public function getScheduleLocationAddress() {
+		return ($this->scheduleLocationAddress);
+	}
 
+	/**
+	 * mutator for schedule location address
+	 *
+	 * @return string $newScheduleLocationAddress the string of the location address
+	 * @throws \RangeException if the string is too long
+	 */
+	public function setScheduleLocationAddress(string $newScheduleLocationAddress) {
+		if($newScheduleLocationAddress === null) {
+			$this->scheduleLocationAddress = null;
+			return;
+		}
+		if($newScheduleLocationAddress > 255) {
+			throw(new \RangeException("This Location Address is too long"));
+		}
+		$this->scheduleLocationAddress = $newScheduleLocationAddress;
+	}
+
+	/**
+	 * accessor for schedule start time
+	 * @return \DateTime value of schedule start
+	 */
+	public function getScheduleStartTime() {
+		return ($this->scheduleStartTime);
+	}
+
+	/**
+	 * mutator for schedule start time
+	 * @param \DateTime|string|null $newScheduleStartTime scheduled start time as a DateTime
+	 * @throws \InvalidArgumentException if there is no schedule start time input
+	 */
+	public function setScheduleStartTime($newScheduleStartTime) {
+		if($newScheduleStartTime === null) {
+			throw(new \InvalidArgumentException("Must enter a start time"));
+		}
+
+		try {
+			$newScheduleStartTime = self::validateDateTime($newScheduleStartTime);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), $invalidArgument));
+		}
+		if($newScheduleStartTime = $newScheduleEndTime) /*not sure if this makes sense*/ {
+			throw(new \InvalidArgumentException("The start time cannot be the same as the end time."));
+			/* so event uses range exception to show that the end time cannot come before the start time. Should that also be the case here??? */
+		}
+		$this->scheduleStartTime = $newScheduleStartTime;
+	}
+
+	/**
+	 * Accessor for the schedule end time
+	 * @return \DateTime value of schedule end
+	 */
+	public function getScheduleEndTime() {
+		return ($this->scheduleEndTime);
+	}
+
+	/**
+	 * mutator for schedule End Time
+	 * @param \DateTime|string|null $newScheduleEndTime scheduled start time as a DateTime
+	 * @throws \InvalidArgumentException if there is no schedule start time input
+	 */
+	public function setScheduleEndTime($newScheduleEndTime) {
+		if($newScheduleEndTime === null) {
+			throw(new \InvalidArgumentException("Must enter in an end time"));
+		}
+
+		try {
+			$newScheduleEndTime = self::validateDateTime($newScheduleEndTime);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), $invalidArgument));
+		}
+		if($newScheduleEndTime = $newScheduleStartTime) { /*not sure if this makes sense*/
+
+			throw(new \InvalidArgumentException("The end time cannot be the same as the start time."));
+		}
+		$this->scheduleEndTime = $newScheduleEndTime;
+//------INSERT UPDATE AND DELETE METHODS HERE--------//
+		/**
+		 *
+		 */
+
+	}
 }
